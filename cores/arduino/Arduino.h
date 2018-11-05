605e66c5ced3f9469d59f2a3ca6833d85c1be332
Martino Facchin <m.facchin@arduino.cc>
2018-11-05 19:15:00 +0100

    Start porting

---
+++ cores/arduino/Arduino.h
@@ -0,0 +1,60 @@
+/*
+  Arduino.h - Main include file for the Arduino SDK
+  Copyright (c) 2005-2013 Arduino Team.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef Arduino_h
+#define Arduino_h
+
+// TEMP: include proper CMSIS
+inline void NVIC_SystemReset() {}
+
+#ifdef __cplusplus
+#define PinMode MbedPinMode
+#include "mbed.h"
+#undef PinMode
+#endif
+
+#define PinMode ArduinoPinMode
+#include "api/ArduinoAPI.h"
+#undef PinMode
+#define PinMode ArduinoPinMode
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+// avr-libc defines _NOP() since 1.6.2
+#ifndef _NOP
+#define _NOP() do { __asm__ volatile ("nop"); } while (0)
+#endif
+
+// Get the bit location within the hardware port of the given virtual pin.
+// This comes from the pins_*.c file for the active board configuration.
+//
+// These perform slightly better as macros compared to inline functions
+//
+#define NOT_A_PIN 255
+#define NOT_A_PORT 255
+#define NOT_AN_INTERRUPT 255
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#include "pins_arduino.h"
+#endif
---
+++ cores/arduino/device.h
@@ -0,0 +1,13 @@
+#ifndef MBED_DEVICE_H
+#define MBED_DEVICE_H
+
+#include "cmsis.h"
+#include "PeripheralNames.h"
+#include "PinNames.h"
+
+/* XXX: Added by Arduino */
+typedef struct {
+    PinName  pin;
+} gpio_t;
+
+#endif
