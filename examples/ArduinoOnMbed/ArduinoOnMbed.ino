83ed5127e536ef5d0b320d2c7b9acbd8e2847fc2
Martino Facchin <m.facchin@arduino.cc>
2018-11-12 19:19:38 +0100

    Improve example with Serial

--- examples/ArduinoOnMbed/ArduinoOnMbed.ino
+++ examples/ArduinoOnMbed/ArduinoOnMbed.ino
@@ -1,14 +1,29 @@
-DigitalOut myled(LED1);
+mbed::DigitalOut myled(LED1);
 
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(LED2, OUTPUT);
+  String a = "test";
+  String b = a + 12;
+  String c = a + "18";
+  Serial.begin(115200);
+  delay(100);
+  Serial.println(a);
+  Serial.println(b);
+  Serial.println(c);
+  a += "68";
+  Serial.println(a);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (Serial.available()) {
+    Serial.write(Serial.read());
+  }
+  digitalWrite(LED2, HIGH);
   myled = 1;
-  wait(0.1);
+  delay(1000);
+  digitalWrite(LED2, LOW);
   myled = 0;
-  wait(0.1);
+  delay(1000);
 }
