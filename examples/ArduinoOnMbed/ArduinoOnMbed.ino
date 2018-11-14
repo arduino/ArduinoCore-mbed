50320542e7cab60e248b10adf9fcf05f11d78974
Martino Facchin <m.facchin@arduino.cc>
2018-11-14 16:03:02 +0100

    Update example

--- examples/ArduinoOnMbed/ArduinoOnMbed.ino
+++ examples/ArduinoOnMbed/ArduinoOnMbed.ino
@@ -1,3 +1,6 @@
+#include "WiFi.h"
+#include "WiFiESP8266.h"
+
 mbed::DigitalOut myled(LED1);
 
 void setup() {
@@ -6,20 +9,24 @@ void setup() {
   String a = "test";
   String b = a + 12;
   String c = a + "18";
-  Serial.begin(115200);
+  Serial.begin(921600);
   delay(100);
   Serial.println(a);
   Serial.println(b);
   Serial.println(c);
   a += "68";
   Serial.println(a);
+//delay(2000);
+  printf("now\n");
+WiFi.begin("12132", "sfsfdf");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.available()) {
+  while (Serial.available()) {
     Serial.write(Serial.read());
   }
+  wait(0.1);
   digitalWrite(LED2, HIGH);
   myled = 1;
   delay(1000);
