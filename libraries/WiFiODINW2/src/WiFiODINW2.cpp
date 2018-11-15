b313e9624057bacb24701b10c577e2f6ee3a8763
Martino Facchin <m.facchin@arduino.cc>
2018-11-15 18:59:02 +0100

    ODIN-WiFi: add switch for late init

--- libraries/WiFiODINW2/src/WiFiODINW2.cpp
+++ libraries/WiFiODINW2/src/WiFiODINW2.cpp
@@ -5,5 +5,15 @@ using namespace mbed;
 
 #include "OdinWiFiInterface.h"
 
+#if 0
+static OdinWiFiInterface* wifi_if;
+void* cb() {
+	wifi_if = new OdinWiFiInterface();
+	return wifi_if;
+}
+
+arduino::WiFiClass WiFi(cb);
+#else
 static OdinWiFiInterface wifi_if;
 arduino::WiFiClass WiFi(&wifi_if);
+#endif
