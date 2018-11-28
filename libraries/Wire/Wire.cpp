2f23f1169ad17c966907b0df1b60ac97b8454539
Martino Facchin <m.facchin@arduino.cc>
2018-11-28 16:33:21 +0100

    Initial Wire implementation

---
+++ libraries/Wire/Wire.cpp
@@ -0,0 +1,104 @@
+#include "Wire.h"
+
+arduino::MbedI2C::MbedI2C(int sda, int scl) : _sda(sda), _scl(scl) {}
+
+void arduino::MbedI2C::begin() {
+	master = new mbed::I2C((PinName)_sda, (PinName)_scl);
+}
+
+void arduino::MbedI2C::begin(uint8_t slaveAddr) {
+	slave = new mbed::I2CSlave((PinName)_sda, (PinName)_scl);
+	slave->address(slaveAddr);
+}
+
+void arduino::MbedI2C::end() {
+	if (master != NULL) {
+		delete master;
+	}
+	if (slave != NULL) {
+		delete slave;
+	}
+}
+
+void arduino::MbedI2C::setClock(uint32_t freq) {
+	if (master != NULL) {
+		master->frequency(freq);
+	}
+	if (slave != NULL) {
+		slave->frequency(freq);
+	}
+}
+
+void arduino::MbedI2C::beginTransmission(uint8_t address) {
+	_address = address << 1;
+	txBuffer.clear();
+}
+
+uint8_t arduino::MbedI2C::endTransmission(bool stopBit) {
+	char buf[256];
+	int len = txBuffer.available();
+	for (int i=0; i<len; i++) {
+		buf[i] = txBuffer.read_char();
+	}
+	master->write(_address, buf, len, !stopBit);
+}
+
+uint8_t arduino::MbedI2C::endTransmission(void) {
+	return endTransmission(true);
+}
+
+uint8_t arduino::MbedI2C::requestFrom(uint8_t address, size_t len, bool stopBit) {
+	char buf[256];
+	int ret = master->read(address << 1, buf, len, !stopBit);
+	Serial.println(ret);
+	if (ret != 0) {
+		return 0;
+	}
+	for (int i=0; i<len; i++) {
+		rxBuffer.store_char(buf[i]);
+	}
+	return len;
+}
+
+uint8_t arduino::MbedI2C::requestFrom(uint8_t address, size_t len) {
+	return requestFrom(address, len, true);
+}
+
+size_t arduino::MbedI2C::write(uint8_t data) {
+	txBuffer.store_char(data);
+}
+
+size_t arduino::MbedI2C::write(uint8_t* data, int len) {
+	for (int i=0; i<len; i++) {
+		write(data[i]);
+	}
+}
+
+int arduino::MbedI2C::read() {
+	if (rxBuffer.available()) {
+		return rxBuffer.read_char();
+	}
+	return 0;
+}
+
+int arduino::MbedI2C::available() {
+	return rxBuffer.available();
+}
+
+int arduino::MbedI2C::peek() {
+	return rxBuffer.peek();
+}
+
+void arduino::MbedI2C::flush() {
+}
+
+
+void arduino::MbedI2C::onReceive(void(*)(int)) {}
+void arduino::MbedI2C::onRequest(void(*)(void)) {}
+
+
+#if DEVICE_I2C > 0
+arduino::MbedI2C Wire(I2C_SDA, I2C_SCL);
+#elif DEVICE_I2C > 1
+arduino::MbedI2C Wire1(I2C_SDA1, I2C_SCL1);;
+#endif
---
+++ libraries/Wire/Wire.h
@@ -0,0 +1,70 @@
+/*
+  Copyright (c) 2016 Arduino LLC.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#pragma once
+
+#include "Arduino.h"
+#include "drivers/I2C.h"
+#include "drivers/I2CSlave.h"
+
+namespace arduino {
+
+class MbedI2C : public HardwareI2C
+{
+  public:
+    MbedI2C(int sda, int scl);
+    virtual void begin();
+    virtual void begin(uint8_t address);
+    virtual void end();
+
+    virtual void setClock(uint32_t freq);
+  
+    virtual void beginTransmission(uint8_t address);
+    virtual uint8_t endTransmission(bool stopBit);
+    virtual uint8_t endTransmission(void);
+
+    virtual uint8_t requestFrom(uint8_t address, size_t len, bool stopBit);
+    virtual uint8_t requestFrom(uint8_t address, size_t len);
+
+    virtual void onReceive(void(*)(int));
+    virtual void onRequest(void(*)(void));
+
+    virtual size_t write(uint8_t data);
+    virtual size_t write(uint8_t* data, int len);
+    virtual int read();
+    virtual int peek();
+    virtual void flush();
+    virtual int available();
+
+private:
+    mbed::I2CSlave* slave;
+    mbed::I2C*      master;
+    int _sda;
+    int _scl;
+    int _address;
+    RingBufferN<256> rxBuffer;
+    RingBufferN<256> txBuffer;
+};
+
+}
+
+#if DEVICE_I2C > 0
+extern arduino::MbedI2C Wire;
+#elif DEVICE_I2C > 1
+extern arduino::MbedI2C Wire1;
+#endif
