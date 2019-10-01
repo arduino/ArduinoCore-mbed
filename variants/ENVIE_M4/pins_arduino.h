a758920d01d436d49c3ee23ad95cccd5a34f1a10
Martino Facchin <m.facchin@arduino.cc>
2019-10-01 14:57:30 +0200

    Add BT flow control UART on Envie M4

--- variants/ENVIE_M4/pins_arduino.h
+++ variants/ENVIE_M4/pins_arduino.h
@@ -1,5 +1,12 @@
 #include "mbed_config.h"
 
-#define SERIAL_HOWMANY		1
+#define SERIAL_HOWMANY		2
 #define SERIAL1_TX			SERIAL_TX
 #define SERIAL1_RX			SERIAL_RX
+
+#define SERIAL2_TX			PA_15
+#define SERIAL2_RX			PF_6
+#define SERIAL2_RTS			PF_8
+#define SERIAL2_CTS			PF_9
+
+#define SerialHCI			UART2
