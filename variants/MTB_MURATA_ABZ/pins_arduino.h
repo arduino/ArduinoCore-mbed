7b00ce4129153571e19b76ceb8a94601d2d881df
Martino Facchin <m.facchin@arduino.cc>
2019-10-17 11:41:13 +0200

    [NANOWAN] make it a real Arduino target

--- variants/MTB_MURATA_ABZ/pins_arduino.h
+++ variants/MTB_MURATA_ABZ/pins_arduino.h
@@ -1,6 +1,47 @@
-#define SERIAL_HOWMANY 1
+#pragma once
+#include "mbed_config.h"
+#include <stdint.h>
+#include <macros.h>
+
+#ifndef __PINS_ARDUINO__
+#define __PINS_ARDUINO__
+
+// Number of pins defined in PinDescription array
+#ifdef __cplusplus
+extern "C" unsigned int PINCOUNT_fn();
+#endif
+#define PINS_COUNT           (PINCOUNT_fn())
+#define NUM_DIGITAL_PINS     (18u)
+#define NUM_ANALOG_INPUTS    (4u)
+#define NUM_ANALOG_OUTPUTS   (0u)
+
+// LEDs
+// ----
+#define PIN_LED     (13u)
+#define LED_BUILTIN PIN_LED
+
+// Analog pins
+// -----------
+#define PIN_A0 (14u)
+#define PIN_A1 (15u)
+#define PIN_A2 (16u)
+#define PIN_A3 (17u)
+static const uint8_t A0  = PIN_A0;
+static const uint8_t A1  = PIN_A1;
+static const uint8_t A2  = PIN_A2;
+static const uint8_t A3  = PIN_A3;
+#define ADC_RESOLUTION 12
+
+#define SERIAL_PORT_USBVIRTUAL      SerialUSB
+#define SERIAL_PORT_MONITOR         SerialUSB
+#define SERIAL_PORT_HARDWARE        Serial1
+#define SERIAL_PORT_HARDWARE_OPEN   Serial2
+
+#define SERIAL_HOWMANY 2
 #define SERIAL1_TX PA_2
 #define SERIAL1_RX PA_3
+#define SERIAL2_TX PA_10
+#define SERIAL2_RX PA_11
 
 #define SERIAL_CDC	1
 #define HAS_UNIQUE_ISERIAL_DESCRIPTOR
@@ -31,4 +72,6 @@ uint8_t getUniqueSerialNumber(uint8_t* name);
 #define MBED_CONF_APP_LORA_RXCTL			PA_1
 #define MBED_CONF_APP_LORA_ANT_SWITCH		NC
 #define MBED_CONF_APP_LORA_PWR_AMP_CTL		PC_1
-#define MBED_CONF_APP_LORA_TCXO				PA_12
+#define MBED_CONF_APP_LORA_TCXO				PA_8
+
+#endif // __PINS_ARDUINO__
--- variants/MTB_MURATA_ABZ/variant.cpp
+++ variants/MTB_MURATA_ABZ/variant.cpp
@@ -1,5 +1,38 @@
 #include "Arduino.h"
 
+PinDescription g_APinDescription[] = {
+  // D0 - D7
+  PA_10, NULL, NULL,     // D0/TX
+  PA_11, NULL, NULL,     // D1/RX
+  PA_0,  NULL, NULL,     // D2
+  PB_2,  NULL, NULL,     // D3
+  PB_5,  NULL, NULL,     // D4
+  PB_6,  NULL, NULL,     // D5
+  PB_7,  NULL, NULL,     // D6
+  NC,    NULL, NULL,     // D7
+
+  // D8 - D13
+  NC,    NULL, NULL,     // D8
+  PB_7,  NULL, NULL,     // D9
+  PB_12, NULL, NULL,     // D10
+  PB_15, NULL, NULL,     // D11/MOSI
+  PB_14, NULL, NULL,     // D12/MISO
+  PB_13, NULL, NULL,     // D13/SCK/LED
+
+  // A0 - A7
+  PA_4,  NULL, NULL,     // A0
+  PA_5,  NULL, NULL,     // A1
+  PA_3,  NULL, NULL,     // A2
+  PA_2,  NULL, NULL,     // A3
+  PB_9,  NULL, NULL,     // A4/SDA
+  PB_8,  NULL, NULL,     // A5/SCL
+};
+
+extern "C" {
+  unsigned int PINCOUNT_fn() {
+    return (sizeof(g_APinDescription) / sizeof(g_APinDescription[0]));
+  }
+}
 
 #ifdef SERIAL_CDC
 
@@ -13,9 +46,13 @@ static void utox8(uint32_t val, uint8_t* s) {
   }
 }
 
+#define STM32_UUID ((uint32_t *)0x1FFF7A10)
+
 uint8_t getUniqueSerialNumber(uint8_t* name) {
-  utox8(123456789, name);
-  return 16;
+  utox8(STM32_UUID[0], &name[0]);
+  utox8(STM32_UUID[1], &name[16]);
+  utox8(STM32_UUID[2], &name[32]);
+  return 48;
 }
 
 void _ontouch1200bps_() {
