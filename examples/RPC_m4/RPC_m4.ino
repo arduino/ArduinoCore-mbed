25fc52cf865dbf52a7e4cbfddedcdee98dd826e0
Martino Facchin <m.facchin@arduino.cc>
2019-11-26 14:06:55 +0100

    Envie: Add M4 RPC example

---
+++ examples/RPC_m4/RPC_m4.ino
@@ -0,0 +1,22 @@
+#include "Arduino.h"
+#include "RPC_internal.h"
+
+#define LED_BUILTIN PK_5
+
+void setup() {
+  // put your setup code here, to run once:
+  RPC1.begin();
+  //Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  RPC1.println("test");
+  auto res = RPC1.client.call("add", 12, 45).as<int>();;
+  RPC1.println(res);
+}
