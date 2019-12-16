5881bd842625a65a20b7989e0d1ee530e97f44c4
Alexander Entinger <cto@lxrobotics.com>
2019-12-16 07:16:54 +0100

    Adding minimalic example in order to get the toolchain to include and compile the folder ENVIE_Video_Firefly

---
+++ examples/Envie_Video_Firefly/Envie_Video_Firefly.ino
@@ -0,0 +1,9 @@
+#include <ENVIE_Video_Firefly.h>
+
+void setup() {
+  anx7625_setup();
+}
+
+void loop() {
+
+}
---
+++ libraries/ENVIE_Video_Firefly/ENVIE_Video_Firefly.h
@@ -0,0 +1,6 @@
+#ifndef ENVIE_VIDEO_FIREFLY_H_
+#define ENVIE_VIDEO_FIREFLY_H_
+
+void anx7625_setup(void);
+
+#endif /* ENVIE_VIDEO_FIREFLY_H_ */
