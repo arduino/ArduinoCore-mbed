b7af3c33fc380e50f0e40ed4f8084cf6ea6247b1
Martino Facchin <m.facchin@arduino.cc>
2020-01-03 19:20:48 +0100

    CES: tune demo example

--- examples/Demo_CES_mjpeg/Demo_CES_mjpeg.ino
+++ examples/Demo_CES_mjpeg/Demo_CES_mjpeg.ino
@@ -7,9 +7,6 @@
 #include "AVI_parser.h"
 #include "image.h"
 
-QSPIFBlockDevice block_device(PD_11, PD_12, PF_7, PD_13,
-                              PF_10, PG_6, QSPIF_POLARITY_MODE_0, MBED_CONF_QSPIF_QSPI_FREQ);
-
 struct edid recognized_edid;
 
 mbed::DigitalOut video_on(PK_2);
@@ -17,7 +14,7 @@ mbed::DigitalOut video_rst(PJ_3);
 
 extern "C" uint32_t JPEG_Decode_DMA(JPEG_HandleTypeDef *hjpeg, uint32_t FrameSourceAddress , uint32_t FrameSize, uint32_t DestAddress);
 
-static uint32_t AVI_FILE_ADDRESS = 0; //((uint32_t)(EnvieH7V1_0_4_avi)); //((uint32_t)(0x90000000)); // start of qspi flash, correct me later
+static uint32_t AVI_FILE_ADDRESS = ((uint32_t)(0x90000000)); // start of qspi flash, correct me later
 
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
@@ -32,63 +29,74 @@ extern __IO uint32_t Jpeg_HWDecodingEnd;
 
 AVI_CONTEXT AVI_Handel;  /* AVI Parser Handle*/
 
-int KMP(char *x, int m, char *y, int n, int start=0);
+int KMP(char *x, int m, char *y, int n, int start = 0);
 
 void setup() {
   // put your setup code here, to run once:
-  video_on = 1;
-  delay(10);
-  video_rst = 1;
-  delay(10);
 
-  delay(1000);
+  int ret = -1;
+  while (ret < 0) {
 
-  int ret = anx7625_init(0);
+    video_on = 0;
+    delay(10);
+    video_rst = 0;
+    delay(100);
 
-  if (ret < 0) {
-    printf("anx7625_init returned %d\n", ret);
-    while (1) {}
+    video_on = 1;
+    delay(100);
+    video_rst = 1;
+
+    ret = anx7625_init(0);
+    printf("after init\n");
+
+    if (ret < 0) {
+      printf("anx7625_init returned %d\n", ret);
+
+    }
   }
-
   anx7625_dp_get_edid(0, &recognized_edid);
   //edid_set_framebuffer_bits_per_pixel(&recognized_edid, 16, 0);
   //set_display_mode(&recognized_edid, EDID_MODE_720x480_60Hz);
   //anx7625_dp_start(0, &recognized_edid, EDID_MODE_1280x720_60Hz);
   anx7625_dp_start(0, &recognized_edid);
 
-  block_device.init();
+  delay(1000);
 
-  mbed::bd_size_t sector_size_at_address_0 = block_device.get_erase_size(0);
+  QSPIFBlockDevice* block_device = new QSPIFBlockDevice(PD_11, PD_12, PF_7, PD_13,
+      PF_10, PG_6, QSPIF_POLARITY_MODE_0, MBED_CONF_QSPIF_QSPI_FREQ);
+  block_device->init();
 
-  printf("QSPIF BD size: %llu\n",         block_device.size());
-  printf("QSPIF BD read size: %llu\n",    block_device.get_read_size());
-  printf("QSPIF BD program size: %llu\n", block_device.get_program_size());
+  mbed::bd_size_t sector_size_at_address_0 = block_device->get_erase_size(0);
+
+  printf("QSPIF BD size: %llu\n",         block_device->size());
+  printf("QSPIF BD read size: %llu\n",    block_device->get_read_size());
+  printf("QSPIF BD program size: %llu\n", block_device->get_program_size());
 
   printf("QSPIF BD erase size (at address 0): %llu\n", sector_size_at_address_0);
 
-/*
-  char avi_header[] = {0x52, 0x49, 0x46, 0x46};
-  char *buffer = (char *) malloc(sector_size_at_address_0);
-  int i = 2048;
-  int res = 0;
-  while (sector_size_at_address_0 * i < block_device.size()) {
-    block_device.read(buffer, sector_size_at_address_0 * i, sector_size_at_address_0);
-    res = KMP(avi_header, 4, buffer, sector_size_at_address_0);
-    if (res != -1) {
-       break;
+  /*
+    char avi_header[] = {0x52, 0x49, 0x46, 0x46};
+    char *buffer = (char *) malloc(sector_size_at_address_0);
+    int i = 2048;
+    int res = 0;
+    while (sector_size_at_address_0 * i < block_device.size()) {
+      block_device.read(buffer, sector_size_at_address_0 * i, sector_size_at_address_0);
+      res = KMP(avi_header, 4, buffer, sector_size_at_address_0);
+      if (res != -1) {
+         break;
+      }
+      i++;
     }
-    i++;
-  }
-*/
+  */
   char *buffer = (char *) malloc(sector_size_at_address_0);
-  block_device.read(buffer, 2048 * 4096, sector_size_at_address_0);
+  block_device->read(buffer, 2048 * 4096, sector_size_at_address_0);
 
-  AVI_FILE_ADDRESS = (((uint32_t)(0x90000000 + (2048 * 4096))));
-  DumpHex((void*)AVI_FILE_ADDRESS, 40);
+  AVI_FILE_ADDRESS = (((uint32_t)(EnvieH7V1_0_4_avi))); // + (2048 * 4096))));
+  //DumpHex((void*)AVI_FILE_ADDRESS, 40);
   printf("Address: %x\n",  AVI_FILE_ADDRESS);
 
   // Deinitialize the device
-  //block_device.deinit();
+  block_device->deinit();
 
   LCD_X_Size = stm32_getXSize();
   LCD_Y_Size = stm32_getYSize();
