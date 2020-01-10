3658aab03618d04736a4411b7ab02eb5ca99ecc6
Martino Facchin <m.facchin@arduino.cc>
2020-01-10 14:23:30 +0100

    Envie_M7: enable USB support

--- variants/ENVIE_M7/pins_arduino.h
+++ variants/ENVIE_M7/pins_arduino.h
@@ -3,3 +3,14 @@
 #define SERIAL_HOWMANY		1
 #define SERIAL1_TX			SERIAL_TX
 #define SERIAL1_RX			SERIAL_RX
+
+#define SERIAL_CDC			1
+#define HAS_UNIQUE_ISERIAL_DESCRIPTOR
+#define BOARD_VENDORID		0x2341
+#define BOARD_PRODUCTID		0x025b
+#define BOARD_NAME			"Envie M7"
+
+#define DFU_MAGIC_SERIAL_ONLY_RESET   0xb0
+
+uint8_t getUniqueSerialNumber(uint8_t* name);
+void _ontouch1200bps_();
---
+++ variants/ENVIE_M7/variant.cpp
@@ -0,0 +1,34 @@
+#include "Arduino.h"
+
+RTC_HandleTypeDef RTCHandle;
+
+void initVariant() {
+	RTCHandle.Instance = RTC;
+	// Turn off LED red from bootloader
+	digitalWrite(PK_6, HIGH);
+}
+
+#ifdef SERIAL_CDC
+
+static void utox8(uint32_t val, uint8_t* s) {
+  for (int i = 0; i < 16; i=i+2) {
+    int d = val & 0XF;
+    val = (val >> 4);
+
+    s[15 - i -1] = d > 9 ? 'A' + d - 10 : '0' + d;
+    s[15 - i] = '\0';
+  }
+}
+
+uint8_t getUniqueSerialNumber(uint8_t* name) {
+  utox8(HAL_GetUIDw0(), &name[0]);
+  utox8(HAL_GetUIDw1(), &name[16]);
+  return 32;
+}
+
+void _ontouch1200bps_() {
+  HAL_RTCEx_BKUPWrite(&RTCHandle, RTC_BKP_DR0, 0xDF59);
+  NVIC_SystemReset();
+}
+
+#endif
