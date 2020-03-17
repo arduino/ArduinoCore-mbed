f904d59ffcee36b560c489729f786b33c299883e
Adam Green <1060664+adamgreen@users.noreply.github.com>
2020-03-16 23:57:29 -0700

    Fix typos & grammatical errors
    Update libraries/ThreadMRI/library.properties
    Co-Authored-By: per1234 <accounts@perglass.com>
    
    Update libraries/ThreadMRI/examples/ThreadMRI/ThreadMRI.ino
    Co-Authored-By: per1234 <accounts@perglass.com>

--- libraries/ThreadMRI/examples/ThreadMRI/ThreadMRI.ino
+++ libraries/ThreadMRI/examples/ThreadMRI/ThreadMRI.ino
@@ -1,5 +1,5 @@
 /* This example demonstrates how to include the MRI library which
-   allows to debug the Portenta H7 Cortex-M7 core with GDB via a
+   allows debugging the Portenta H7 Cortex-M7 core with GDB via a
    serial interface.
 */
 
