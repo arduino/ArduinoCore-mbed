e4b04bf2291d98785cb6999ef5574593dbd2195d
Martino Facchin <m.facchin@arduino.cc>
2020-03-24 12:51:02 +0100

    Add PortentaH7 bootloader drivers

--- post_install.bat
+++ post_install.bat
@@ -1,5 +1,12 @@
 @echo off
 set ARGS=/SE /SW /SA
+if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+  drivers\dpinst-amd64.exe %ARGS%
+) ELSE IF "%PROCESSOR_ARCHITEW6432%" == "AMD64" (
+  drivers\dpinst-amd64.exe %ARGS%
+) ELSE (
+  drivers\dpinst-x86.exe %ARGS%
+)
 
 @echo off
 setlocal
