db20615b08c2b9063b0acbd4d37c9f21b0272161
Martino Facchin <m.facchin@arduino.cc>
2020-04-08 18:07:28 +0200

    PortentaH7: expose board info from bootloader

---
+++ examples/PortentaH7_getBootloaderInfo/PortentaH7_getBootloaderInfo.ino
@@ -0,0 +1,22 @@
+uint8_t* bootloader_data = (uint8_t*)(0x801F000);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  while (!Serial) {}
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Validation: " + String(bootloader_data[0], HEX));
+  Serial.println("BL version: " + String(bootloader_data[1]));
+  Serial.println("Clock source: " + String(bootloader_data[2]));
+  Serial.println("USB Speed: " + String(bootloader_data[3]));
+  Serial.println("Ethernet: " + String(bootloader_data[4]));
+  Serial.println("Wifi: " + String(bootloader_data[5]));
+  Serial.println("RAM size: " + String(bootloader_data[6]));
+  Serial.println("QSPI size: " + String(bootloader_data[7]));
+  Serial.println("Video: " + String(bootloader_data[8]));
+  Serial.println("Crypto: " + String(bootloader_data[9]));
+  delay(1000);
+}
