73d4edb2788640f2a92f91f5cf5afca522202a96
Martino Facchin <m.facchin@arduino.cc>
2020-05-19 12:41:22 +0200

    Fix examples documentation for MRI libraries

--- libraries/KernelDebug/examples/KernelDebug/KernelDebug.ino
+++ libraries/KernelDebug/examples/KernelDebug/KernelDebug.ino
@@ -4,7 +4,7 @@
 
    To connect to the target, launch gdb with the following parameters
  
-   arm-none-eabi-gdb --interpreter=mi2 -ex "set pagination off" --baud {230400} -ex "set target-charset ASCII" -ex "target remote {debug.port}" {project_name}.elf
+   arm-none-eabi-gdb -ex "set pagination off" --baud {230400} -ex "set target-charset ASCII" -ex "target remote {debug.port}" {project_name}.elf
 
    The baud rate needs to match the one provided in KernelDebug constructor, while {debug.port} depends on the operating system (eg. /dev/ttyUSB0 or COM15)
 */
--- libraries/ThreadDebug/examples/ThreadDebug/ThreadDebug.ino
+++ libraries/ThreadDebug/examples/ThreadDebug/ThreadDebug.ino
@@ -1,5 +1,12 @@
-/* This example demonstrates how to include the ThreadMRI library which allows debugging of the Portenta H7
-   with GDB via a serial interface.
+/* This example demonstrates how to include the ThreadMRI library which allows debugging of the Portenta H7 with GDB via a serial interface.
+
+   To connect to the target, launch gdb with the following parameters
+
+   arm-none-eabi-gdb -ex "set pagination off" --baud {230400} -ex "set target-charset ASCII" -ex "target remote {debug.port}" {project_name}.elf
+
+   The baud rate needs to match the one provided in UartDebugCommInterface constructor, while {debug.port} depends on the operating system (eg. /dev/ttyUSB0 or COM15).
+
+   If UsbDebugCommInterface is being used you can specify any baudrate.
 */
 
 #include <ThreadDebug.h>
