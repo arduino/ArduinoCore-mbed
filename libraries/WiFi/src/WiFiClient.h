d32d28267675bc62262fef892a68e4b7a8e43ffd
Sebastian Hunkeler <s.hunkeler@arduino.cc>
2020-08-12 11:30:08 +0200

    Add IPAddress support to connectSSL

--- libraries/WiFi/src/WiFiClient.h
+++ libraries/WiFi/src/WiFiClient.h
@@ -38,6 +38,7 @@ public:
   int connect(SocketAddress socketAddress);
   int connect(IPAddress ip, uint16_t port);
   int connect(const char *host, uint16_t port);
+  int connectSSL(SocketAddress socketAddress);
   int connectSSL(IPAddress ip, uint16_t port);
   int connectSSL(const char *host, uint16_t port);
   size_t write(uint8_t);
