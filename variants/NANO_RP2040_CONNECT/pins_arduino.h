3aa6138b89098e511dd1d01c14f3b53882317c8a
Martino Facchin <m.facchin@arduino.cc>
2021-02-26 11:56:57 +0100

    RP2040: properly define pins for production board

--- variants/NANO_RP2040_CONNECT/pins_arduino.h
+++ variants/NANO_RP2040_CONNECT/pins_arduino.h
@@ -12,24 +12,24 @@ extern "C" unsigned int PINCOUNT_fn();
 // Pin count
 // ----
 #define PINS_COUNT           (PINCOUNT_fn())
-#define NUM_DIGITAL_PINS     (32u)
+#define NUM_DIGITAL_PINS     (30u)
 #define NUM_ANALOG_INPUTS    (4u)
 #define NUM_ANALOG_OUTPUTS   (0u)
 
 // LEDs
 // ----
-#define PIN_LED     (25u)
+#define PIN_LED     (13u)
 #define LED_BUILTIN PIN_LED
-#define LEDR        (22u)
-#define LEDG        (23u)
-#define LEDB        (24u)
+#define LEDR        (20u)
+#define LEDG        (21u)
+#define LEDB        (13u)
 
 // Analog pins
 // -----------
-#define PIN_A0 (26u)
-#define PIN_A1 (27u)
-#define PIN_A2 (28u)
-#define PIN_A3 (29u)
+#define PIN_A0 (14u)
+#define PIN_A1 (15u)
+#define PIN_A2 (16u)
+#define PIN_A3 (17u)
 /*
 static const uint8_t A0  = PIN_A0;
 static const uint8_t A1  = PIN_A1;
@@ -43,10 +43,10 @@ static const uint8_t A3  = PIN_A3;
 #define PIN_SERIAL_TX (0ul)
 
 // SPI
-#define PIN_SPI_MISO  (10u)
-#define PIN_SPI_MOSI  (8u)
-#define PIN_SPI_SCK   (9u)
-#define PIN_SPI_SS    (7u)
+#define PIN_SPI_MISO  (12u)
+#define PIN_SPI_MOSI  (11u)
+#define PIN_SPI_SCK   (13u)
+#define PIN_SPI_SS    (10u)
 
 static const uint8_t SS   = PIN_SPI_SS;   // SPI Slave SS not used. Set here only for reference.
 static const uint8_t MOSI = PIN_SPI_MOSI;
@@ -54,19 +54,16 @@ static const uint8_t MISO = PIN_SPI_MISO;
 static const uint8_t SCK  = PIN_SPI_SCK;
 
 // Wire
-#define PIN_WIRE_SDA        (11u)
-#define PIN_WIRE_SCL        (12u)
+#define PIN_WIRE_SDA        (18u)
+#define PIN_WIRE_SCL        (19u)
 
-#define SERIAL_HOWMANY		3
+#define SERIAL_HOWMANY		2
 #define SERIAL1_TX			(digitalPinToPinName(PIN_SERIAL_TX))
 #define SERIAL1_RX			(digitalPinToPinName(PIN_SERIAL_RX))
-#define SERIAL2_TX			(8)
-#define SERIAL2_RX			(9)
-#define SERIAL2_CTS			(10)
-#define SERIAL2_RTS			(11)
-#define SERIAL3_HW			(uart1)
-#define SERIAL3_TX			(8)
-#define SERIAL3_RX			(9)
+#define SERIAL2_TX			(digitalPinToPinName(25))
+#define SERIAL2_RX			(digitalPinToPinName(26))
+#define SERIAL2_CTS			(digitalPinToPinName(27))
+#define SERIAL2_RTS			(digitalPinToPinName(28))
 
 #define SERIAL_CDC			1
 #define HAS_UNIQUE_ISERIAL_DESCRIPTOR
@@ -78,21 +75,32 @@ uint8_t getUniqueSerialNumber(uint8_t* name);
 void _ontouch1200bps_();
 
 #define SPI_HOWMANY		(2)
-#define SPI_HW			(spi0)
-#define SPI_MISO		(4)
-#define SPI_MOSI		(7)
-#define SPI_SCK			(6)
-#define SPI1_HW			(spi1)
-#define SPI1_MISO		(8)
-#define SPI1_MOSI		(11)
-#define SPI1_SCK		(14)
-#define SPI1_CS			(9)
-#define SPI1_ACK		(10)
+#define SPI_MISO		(digitalPinToPinName(PIN_SPI_MISO))
+#define SPI_MOSI		(digitalPinToPinName(PIN_SPI_MOSI))
+#define SPI_SCK			(digitalPinToPinName(PIN_SPI_SCK))
+
+#define SPI_MISO1		(digitalPinToPinName(25))
+#define SPI_MOSI1		(digitalPinToPinName(28))
+#define SPI_SCK1		(digitalPinToPinName(29))
+
+#define NINA_RESETN		(24u)
+#define SerialNina		Serial2
+
+#if __has_include (<WiFiNINA.h>)
+#define NINA_GPIO0		(21u) // LEDG pin (GPIO26 on NINA)
+#else
+#define NINA_GPIO0		(28u) // real GPIO0 on NINA
+#endif
+
+#define SPIWIFI_SS		(26u)
+#define SPIWIFI_ACK		(27u)
+#define SPIWIFI_RESET	(NINA_RESETN)
+#define SPIWIFI 		(SPI1)
 
 #define WIRE_HOWMANY	(1)
 #define I2C_HW			(i2c0)
-#define I2C_SDA			(12)
-#define I2C_SCL			(13)
+#define I2C_SDA			(digitalPinToPinName(PIN_WIRE_SDA))
+#define I2C_SCL			(digitalPinToPinName(PIN_WIRE_SCL))
 
 #define digitalPinToPort(P)		(digitalPinToPinName(P)/32)
 
--- variants/NANO_RP2040_CONNECT/variant.cpp
+++ variants/NANO_RP2040_CONNECT/variant.cpp
@@ -11,38 +11,45 @@ PinDescription g_APinDescription[] = {
   // D0 - D7
   { p0,         NULL, NULL, NULL },    // D0
   { p1,         NULL, NULL, NULL },    // D1
-  { p2,         NULL, NULL, NULL },    // D2
-  { p3,         NULL, NULL, NULL },    // D3
-  { p4,         NULL, NULL, NULL },    // D4
-  { p5,         NULL, NULL, NULL },    // D5
-  { p6,         NULL, NULL, NULL },    // D6
-  { p7,         NULL, NULL, NULL },    // D7
+  { p25,        NULL, NULL, NULL },    // D2
+  { p15,        NULL, NULL, NULL },    // D3
+  { p16,        NULL, NULL, NULL },    // D4
+  { p17,        NULL, NULL, NULL },    // D5
+  { p18,        NULL, NULL, NULL },    // D6
+  { p19,        NULL, NULL, NULL },    // D7
 
-  // D8 - D14
-  { p8,         NULL, NULL, NULL },    // D8
-  { p9,         NULL, NULL, NULL },    // D9
-  { p10,        NULL, NULL, NULL },    // D10
-  { p11,        NULL, NULL, NULL },    // D11
-  { p12,        NULL, NULL, NULL },    // D12
-  { p13,        NULL, NULL, NULL },    // D13
-  { p14,        NULL, NULL, NULL },    // D14
+  // D8 - D13
+  { p20,        NULL, NULL, NULL },    // D8
+  { p21,        NULL, NULL, NULL },    // D9
+  { p5,         NULL, NULL, NULL },    // D10
+  { p7,         NULL, NULL, NULL },    // D11 / SPITX
+  { p4,         NULL, NULL, NULL },    // D12 / SPIRX
+  { p6,         NULL, NULL, NULL },    // D13 / SPICLK / LEDB 
 
-  // D15 - D31
-  { p15,        NULL, NULL, NULL },    // D15
-  { p16,        NULL, NULL, NULL },    // D16
-  { p17,        NULL, NULL, NULL },    // D17
-  { p18,        NULL, NULL, NULL },    // D18
-  { p19,        NULL, NULL, NULL },    // D19
-  { p20,        NULL, NULL, NULL },    // D20
-  { p21,        NULL, NULL, NULL },    // D21
-  { p22,        NULL, NULL, NULL },    // D22
-  { p23,        NULL, NULL, NULL },    // D23
-  { p24,        NULL, NULL, NULL },    // D24
-  { p25,        NULL, NULL, NULL },    // D25
-  { p26,        NULL, NULL, NULL },    // D26
-  { p27,        NULL, NULL, NULL },    // D27
-  { p28,        NULL, NULL, NULL },    // D28
-  { p29,        NULL, NULL, NULL },    // D29
+  // Analog as digital
+  // A4 to A7 are controlled by Nina module and exposed via different APIs
+  { p26,        NULL, NULL, NULL },    // A0 -> D14
+  { p27,        NULL, NULL, NULL },    // A1 -> D15
+  { p28,        NULL, NULL, NULL },    // A2 -> D16
+  { p29,        NULL, NULL, NULL },    // A3 -> D17
+
+  // I2C
+  { p12,        NULL, NULL, NULL },    // A4 / SDA  -> D18
+  { p13,        NULL, NULL, NULL },    // A5 / SCL  -> D19
+
+  // Internal pins - D20 - D23
+  { p2,         NULL, NULL, NULL },    // LEDR / IMU IRQ1 / to NINA
+  { p24,        NULL, NULL, NULL },    // LEDG / IMU IRQ2 / to NINA
+  { p22,        NULL, NULL, NULL },    // PDM DATA IN
+  { p23,        NULL, NULL, NULL },    // PDM CLOCK
+
+  // Internal pins Nina - D24 - D29
+  { p3,         NULL, NULL, NULL },    // RESET_NINA
+  { p8,         NULL, NULL, NULL },    // SPI1_CIPO / UART1_TX
+  { p9,         NULL, NULL, NULL },    // SPI1_CS / UART1_RX
+  { p10,        NULL, NULL, NULL },    // SPI1_ACK / UART1_CTS
+  { p11,        NULL, NULL, NULL },    // SPI1_COPI / UART1_RTS
+  { p14,        NULL, NULL, NULL },    // SPI1_SCK
 };
 
 extern "C" {
