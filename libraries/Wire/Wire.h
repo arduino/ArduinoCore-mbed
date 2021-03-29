2958bc00a985cbe5b63400fb4afa117453750255
Martino Facchin <m.facchin@arduino.cc>
2021-03-29 12:03:32 +0200

    Wire: fix write(0) overload

--- libraries/Wire/Wire.h
+++ libraries/Wire/Wire.h
@@ -56,6 +56,9 @@ class MbedI2C : public HardwareI2C
     virtual void onRequest(void(*)(void));
 
     virtual size_t write(uint8_t data);
+    virtual size_t write(int data) {
+      return write ((uint8_t)data);
+    };
     virtual size_t write(const uint8_t* data, int len);
     using Print::write;
     virtual int read();
