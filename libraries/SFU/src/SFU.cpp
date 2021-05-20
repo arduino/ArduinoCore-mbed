287ee93dcf313e0d7596621858570e12642578c2
Martino Facchin <m.facchin@arduino.cc>
2021-05-20 15:19:23 +0200

    SFU: don't allocate PluggableMassStorage

--- libraries/SFU/src/SFU.cpp
+++ libraries/SFU/src/SFU.cpp
@@ -1,32 +1,20 @@
 #include "SFU.h"
 #include "FlashIAPBlockDevice.h"
 #include "FATFileSystem.h"
-#include "PluggableUSBMSD.h"
+//#include "PluggableUSBMSD.h"
 
 const unsigned char SFU[0x10000] __attribute__ ((section(".second_stage_ota"), used)) = {
 	#include "rp2040.h"
 };
 
 FlashIAPBlockDevice bd(XIP_BASE + 0x100000, 0x100000);
-
-void USBMSD::begin()
-{
-    int err = getFileSystem().mount(&bd);
-    if (err) {
-        err = getFileSystem().reformat(&bd);
-    }
-}
-
-mbed::FATFileSystem& USBMSD::getFileSystem()
-{
-	static mbed::FATFileSystem fs("ota");
-	return fs;
-}
-
-USBMSD MassStorage(&bd);
+static mbed::FATFileSystem fs("ota");
 
 int SFU::begin() {
-	MassStorage.begin();
+	int err = fs.mount(&bd);
+	if (err) {
+        err = fs.reformat(&bd);
+    }
 }
 
 int SFU::download(const char* url) {
--- libraries/SFU/src/SFU.h
+++ libraries/SFU/src/SFU.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "WiFiNINA.h"
 
 class SFU {
