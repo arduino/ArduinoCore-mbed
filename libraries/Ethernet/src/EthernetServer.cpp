d9fa6255980db6c8736b7a6dc83f8fe624ff53cd
Giampaolo Mancini <giampaolo@trampolineup.com>
2021-07-13 09:27:03 +0200

    Add EthernetServer::available()

---
+++ libraries/Ethernet/src/EthernetServer.cpp
@@ -0,0 +1,18 @@
+#include "EthernetServer.h"
+
+arduino::EthernetClient arduino::EthernetServer::available(uint8_t* status) {
+  EthernetClient client;
+  nsapi_error_t error;
+
+  if (sock == nullptr) {
+    return client;
+  }
+  TCPSocket* clientSocket = sock->accept(&error);
+  if (status != nullptr) {
+    *status = error == NSAPI_ERROR_OK ? 1 : 0;
+  }
+  if (error == NSAPI_ERROR_OK) {
+    client.setSocket(clientSocket);
+  }
+  return client;
+}
