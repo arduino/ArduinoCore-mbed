a64e6fa430651b0ba4f586dd2e5e600b5c9b3ce7
Sebastian Romero <s.romero@arduino.cc>
2022-02-16 13:26:42 +0100

    Reconnect serial port when disconnected

--- libraries/Camera/examples/CameraCaptureRawBytes/CameraCaptureRawBytes.ino
+++ libraries/Camera/examples/CameraCaptureRawBytes/CameraCaptureRawBytes.ino
@@ -33,9 +33,7 @@ void blinkLED(uint32_t count = 0xFFFFFFFF)
   }
 }
 
-void setup() {
-  Serial.begin(921600);
-
+void setup() {   
   // Init the cam QVGA, 30FPS
   if (cam.begin(CAMERA_R320x240, IMAGE_MODE, 30) != 0) {
     blinkLED();
@@ -45,8 +43,12 @@ void setup() {
 }
 
 void loop() {
+  if(!Serial) {    
+    Serial.begin(921600);
+    while(!Serial);
+  }
 
-  // Time out after 2 seconds and ask for new data
+  // Time out after 2 seconds and send new data
   bool timeoutDetected = millis() - lastUpdate > 2000;
   
   // Wait for sync byte.
@@ -58,6 +60,6 @@ void loop() {
   if (cam.GrabFrame(fb, 3000) == 0) {
     Serial.write(fb.getBuffer(), cam.FrameSize());
   } else {
-    blinkLED();
+    blinkLED(20);
   }
 }
