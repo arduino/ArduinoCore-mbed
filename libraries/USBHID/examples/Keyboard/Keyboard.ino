3d68781a3883a60599d6e8c2b88a58507a87803a
Boyd Kane <33420535+beyarkay@users.noreply.github.com>
2022-03-13 13:32:55 +0200

    Fix: short delay prevented new code uploads.
    When the Arduino is treated as a keyboard and automatically typed `Hello world\n\r` every second, it was nearly impossible to upload new code due to the typing interfering with manually choosing the correct port. See [this issue](https://github.com/arduino/ArduinoCore-mbed/issues/424) for more details.

--- libraries/USBHID/examples/Keyboard/Keyboard.ino
+++ libraries/USBHID/examples/Keyboard/Keyboard.ino
@@ -9,7 +9,9 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(1000);
+  // Every 30 seconds, write `Hello world\n\r` to the connected computer as though the arduino were a keyboard.
+  // WARNING: Decreasing this delay could render the arduino useless, as the frequent newlines will make uploading new code 
+  //          nearly impossible. See [this issue](https://github.com/arduino/ArduinoCore-mbed/issues/424) for details
+  delay(30000);
   Keyboard.printf("Hello world\n\r");
 }
