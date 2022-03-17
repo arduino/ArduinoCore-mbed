208093822b8015a68573d08e74c7178666ce75e2
giulcioffi <g.cioffi@arduino.cc>
2022-03-17 16:40:04 +0100

    Add GIGA target - initial

---
+++ variants/GIGA/pins_arduino.h
@@ -0,0 +1,251 @@
+#pragma once
+#include <macros.h>
+#include "PeripheralPins.h"
+
+#ifndef __PINS_ARDUINO__
+#define __PINS_ARDUINO__
+
+#ifdef __cplusplus
+extern "C" unsigned int PINCOUNT_fn();
+extern "C" bool isBetaBoard();
+#endif
+
+// Booting
+// ----
+#define bootM4() LL_RCC_ForceCM4Boot() // Provide a memorable alias
+
+extern PinName digitalPinToPinName(pin_size_t P);
+
+// Pin count
+// ----
+#define PINS_COUNT           (PINCOUNT_fn())
+#define NUM_DIGITAL_PINS     (103u)
+#define NUM_ANALOG_INPUTS    (14u)
+#define NUM_ANALOG_OUTPUTS   (2u)
+
+// LEDs
+// ----
+#define PIN_LED     (87u)
+#define LED_BUILTIN PIN_LED
+#define LEDR        (86u)
+#define LEDG        (87u)
+#define LEDB        (88u)
+
+// Analog pins
+// -----------
+#define PIN_A0 (76u)
+#define PIN_A1 (77u)
+#define PIN_A2 (78u)
+#define PIN_A3 (79u)
+#define PIN_A4 (80u)
+#define PIN_A5 (81u)
+#define PIN_A6 (82u)
+#define PIN_A7 (83u)
+#define PIN_A12 (84u)
+#define PIN_A13 (85u)
+
+static const uint8_t A0  = PIN_A0;
+static const uint8_t A1  = PIN_A1;
+static const uint8_t A2  = PIN_A2;
+static const uint8_t A3  = PIN_A3;
+static const uint8_t A4  = PIN_A4;
+static const uint8_t A5  = PIN_A5;
+static const uint8_t A6  = PIN_A6;
+static const uint8_t A7  = PIN_A7;
+static const uint8_t A12  = PIN_A12;
+static const uint8_t A13  = PIN_A13;
+#define ADC_RESOLUTION 12
+
+// Digital pins
+// -----------
+#define D0  (0u)
+#define D1  (1u)
+#define D2  (2u)
+#define D3  (3u)
+#define D4  (4u)
+#define D5  (5u)
+#define D6  (6u)
+#define D7  (7u)
+#define D8  (8u)
+#define D9  (9u)
+#define D10 (10u)
+#define D11 (11u)
+#define D12 (12u)
+#define D13 (13u)
+#define D14 (14u)
+#define D15 (15u)
+#define D16 (16u)
+#define D17 (17u)
+#define D18 (18u)
+#define D19 (19u)
+#define D20 (20u)
+#define D21 (21u)
+#define D22 (22u)
+#define D23 (23u)
+#define D24 (24u)
+#define D25 (25u)
+#define D26 (26u)
+#define D27 (27u)
+#define D28 (28u)
+#define D29 (29u)
+#define D30 (30u)
+#define D31 (31u)
+#define D32 (32u)
+#define D33 (33u)
+#define D34 (34u)
+#define D35 (35u)
+#define D36 (36u)
+#define D37 (37u)
+#define D38 (38u)
+#define D39 (39u)
+#define D40 (40u)
+#define D41 (41u)
+#define D42 (42u)
+#define D43 (43u)
+#define D44 (44u)
+#define D45 (45u)
+#define D46 (46u)
+#define D47 (47u)
+#define D48 (48u)
+#define D49 (49u)
+#define D50 (50u)
+#define D51 (51u)
+#define D52 (52u)
+#define D53 (53u)
+#define D54 (54u)
+#define D55 (55u)
+#define D56 (56u)
+#define D57 (57u)
+#define D58 (58u)
+#define D59 (59u)
+#define D60 (60u)
+#define D61 (61u)
+#define D62 (62u)
+#define D63 (63u)
+#define D64 (64u)
+#define D65 (65u)
+#define D66 (66u)
+#define D67 (67u)
+#define D68 (68u)
+#define D69 (69u)
+#define D70 (70u)
+#define D71 (71u)
+#define D72 (72u)
+#define D73 (73u)
+#define D74 (74u)
+#define D75 (75u)
+#define D76 (76u)
+#define D77 (77u)
+#define D78 (78u)
+#define D79 (79u)
+#define D80 (80u)
+#define D81 (81u)
+#define D82 (82u)
+#define D83 (83u)
+#define D84 (84u)
+#define D85 (85u)
+#define D86 (86u)
+#define D87 (87u)
+#define D88 (88u)
+#define D89 (89u)
+#define D90 (90u)
+#define D91 (91u)
+#define D92 (92u)
+#define D93 (93u)
+#define D94 (94u)
+#define D95 (95u)
+#define D96 (96u)
+#define D97 (97u)
+#define D98 (98u)
+#define D99 (99u)
+#define D100 (100u)
+#define D101 (101u)
+
+// SPI
+#define PIN_SPI_MISO  (12u)
+#define PIN_SPI_MOSI  (11u)
+#define PIN_SPI_SCK   (13u)
+#define PIN_SPI_SS    (10u)
+
+#define PIN_SPI_MISO1  (89u)
+#define PIN_SPI_MOSI1  (90u)
+#define PIN_SPI_SCK1   (91u)
+#define PIN_SPI_SS1    (92u)
+
+static const uint8_t SS   = PIN_SPI_SS;   // SPI Slave SS not used. Set here only for reference.
+static const uint8_t MOSI = PIN_SPI_MOSI;
+static const uint8_t MISO = PIN_SPI_MISO;
+static const uint8_t SCK  = PIN_SPI_SCK;
+
+// QSPI
+#define QSPI_SO0        PD_11
+#define QSPI_SO1        PD_12
+#define QSPI_SO2        PE_2
+#define QSPI_SO3        PF_6
+#define QSPI_SCK        PF_10
+#define QSPI_CS         PG_6
+
+#define SERIAL_HOWMANY		4
+
+// Serial
+#define SERIAL1_RX 			(0ul)
+#define SERIAL1_TX 			(1ul)
+
+#define SERIAL2_TX			(18u)
+#define SERIAL2_RX			(19u)
+
+#define SERIAL3_TX			(16u)
+#define SERIAL3_RX			(17u)
+
+#define SERIAL4_TX			(14u)
+#define SERIAL4_RX			(15u)
+
+#define SERIAL_CDC			1
+#define HAS_UNIQUE_ISERIAL_DESCRIPTOR
+#define BOARD_VENDORID		0x2341
+#define BOARD_PRODUCTID		0x0266
+#define BOARD_NAME			"Giga"
+
+#define DFU_MAGIC_SERIAL_ONLY_RESET   0xb0
+
+uint8_t getUniqueSerialNumber(uint8_t* name);
+void _ontouch1200bps_();
+
+// Wire
+
+#define WIRE_HOWMANY		3
+
+#define I2C_SDA				(digitalPinToPinName(20))
+#define I2C_SCL				(digitalPinToPinName(21))
+
+#define I2C_SDA1			(PH_12)
+#define I2C_SCL1			(PB_6_ALT0)
+
+#define I2C_SDA2			(digitalPinToPinName(9))
+#define I2C_SCL2			(digitalPinToPinName(8))
+
+#define SPI_HOWMANY			2
+
+#define SPI_MISO			(digitalPinToPinName(PIN_SPI_MISO))
+#define SPI_MOSI			(digitalPinToPinName(PIN_SPI_MOSI))
+#define SPI_SCK				(digitalPinToPinName(PIN_SPI_SCK))
+#define SPI_SS        (digitalPinToPinName(PIN_SPI_SS))
+
+#define SPI_MISO1     (digitalPinToPinName(PIN_SPI_MISO1))
+#define SPI_MOSI1     (digitalPinToPinName(PIN_SPI_MOSI1))
+#define SPI_SCK1      (digitalPinToPinName(PIN_SPI_SCK1))
+#define SPI_SS1       (digitalPinToPinName(PIN_SPI_SS1))
+
+//#define LSM6DS_DEFAULT_SPI SPI1
+//#define LSM6DS_INT         PA_1
+
+#define digitalPinToPort(P)		(digitalPinToPinName(P)/32)
+
+#define SERIAL_PORT_USBVIRTUAL      SerialUSB
+#define SERIAL_PORT_MONITOR         SerialUSB
+#define SERIAL_PORT_HARDWARE        Serial1
+#define SERIAL_PORT_HARDWARE_OPEN   Serial2
+
+#define USB_MAX_POWER	(500)
+
+#endif //__PINS_ARDUINO__
---
+++ variants/GIGA/variant.cpp
@@ -0,0 +1,202 @@
+#include "Arduino.h"
+#include "pinDefinitions.h"
+
+RTC_HandleTypeDef RTCHandle;
+
+AnalogPinDescription g_AAnalogPinDescription[] = {
+  { PC_4,         NULL },    // A0   ADC1_INP4
+  { PC_5,         NULL },    // A1   ADC1_INP8
+  { PB_0,         NULL },    // A2   ADC1_INP9
+  { PB_1,         NULL },    // A3   ADC1_INP5
+  { PC_3,         NULL },    // A4   ADC1_INP13
+  { PC_2,         NULL },    // A5   ADC1_INP12
+  { PC_0,         NULL },    // A6   ADC1_INP10
+  { PA_0,         NULL },    // A7   ADC1_INP16
+  { PC_2C,        NULL },    // A8   ADC3_INP0
+  { PC_3C,        NULL },    // A9   ADC3_INP1
+  { PA_1C,        NULL },    // A10  ADC2_INP1
+  { PA_0C,        NULL },    // A11  ADC2_INP0
+  { PA_4,         NULL },    // A12  DAC1_OUT1
+  { PA_5,         NULL },    // A13  DAC1_OUT2
+};
+
+PinDescription g_APinDescription[] = {
+  // D0 - D21
+  { PB_7,         NULL, NULL, NULL },    // D0  RX
+  { PA_9,         NULL, NULL, NULL },    // D1  TX
+  { PA_3,         NULL, NULL, NULL },    // D2
+  { PA_2,         NULL, NULL, NULL },    // D3
+  { PJ_8,         NULL, NULL, NULL },    // D4
+  { PA_7,         NULL, NULL, NULL },    // D5
+  { PD_13,        NULL, NULL, NULL },    // D6
+  { PB_4,         NULL, NULL, NULL },    // D7
+  { PB_8,         NULL, NULL, NULL },    // D8
+  { PB_9,         NULL, NULL, NULL },    // D9
+  { PK_1,         NULL, NULL, NULL },    // D10
+  { PJ_10,        NULL, NULL, NULL },    // D11
+  { PJ_11,        NULL, NULL, NULL },    // D12
+  { PH_6,         NULL, NULL, NULL },    // D13
+  { PG_14,        NULL, NULL, NULL },    // D14  TX3
+  { PC_7,         NULL, NULL, NULL },    // D15  RX3
+  { PH_13,        NULL, NULL, NULL },    // D16  TX2
+  { PI_9,         NULL, NULL, NULL },    // D17  RX2
+  { PD_5,         NULL, NULL, NULL },    // D18  TX1
+  { PD_6,         NULL, NULL, NULL },    // D19  RX1
+  { PB_11,        NULL, NULL, NULL },    // D20  SDA
+  { PH_4,         NULL, NULL, NULL },    // D21  SCL
+
+  // D22 - D53
+  { PJ_12,        NULL, NULL, NULL },    // D22
+  { PG_13,        NULL, NULL, NULL },    // D23
+  { PG_12,        NULL, NULL, NULL },    // D24
+  { PJ_0,         NULL, NULL, NULL },    // D25
+  { PJ_14,        NULL, NULL, NULL },    // D26
+  { PJ_1,         NULL, NULL, NULL },    // D27
+  { PJ_15,        NULL, NULL, NULL },    // D28
+  { PJ_2,         NULL, NULL, NULL },    // D29
+  { PK_3,         NULL, NULL, NULL },    // D30
+  { PJ_3,         NULL, NULL, NULL },    // D31
+  { PK_4,         NULL, NULL, NULL },    // D32
+  { PJ_4,         NULL, NULL, NULL },    // D33
+  { PK_5,         NULL, NULL, NULL },    // D34
+  { PJ_5,         NULL, NULL, NULL },    // D35
+  { PK_6,         NULL, NULL, NULL },    // D36
+  { PJ_6,         NULL, NULL, NULL },    // D37
+  { PJ_7,         NULL, NULL, NULL },    // D38
+  { PI_14,        NULL, NULL, NULL },    // D39
+  { PE_6,         NULL, NULL, NULL },    // D40
+  { PK_7,         NULL, NULL, NULL },    // D41
+  { PI_15,        NULL, NULL, NULL },    // D42
+  { PI_10,        NULL, NULL, NULL },    // D43
+  { PG_10,        NULL, NULL, NULL },    // D44
+  { PI_13,        NULL, NULL, NULL },    // D45
+  { PH_15,        NULL, NULL, NULL },    // D46
+  { PB_2,         NULL, NULL, NULL },    // D47
+  { PK_0,         NULL, NULL, NULL },    // D48
+  { PE_4,         NULL, NULL, NULL },    // D49
+  { PI_11,        NULL, NULL, NULL },    // D50
+  { PE_5,         NULL, NULL, NULL },    // D51
+  { PK_2,         NULL, NULL, NULL },    // D52
+  { PG_7,         NULL, NULL, NULL },    // D53
+
+  // D54 - D67
+  { PI_5,         NULL, NULL, NULL },    // D54
+  { PH_8,         NULL, NULL, NULL },    // D55
+  { PA_6,         NULL, NULL, NULL },    // D56
+  { PJ_9,         NULL, NULL, NULL },    // D57
+  { PI_7,         NULL, NULL, NULL },    // D58
+  { PI_6,         NULL, NULL, NULL },    // D59
+  { PI_4,         NULL, NULL, NULL },    // D60
+  { PH_14,        NULL, NULL, NULL },    // D61
+  { PG_11,        NULL, NULL, NULL },    // D62
+  { PH_11,        NULL, NULL, NULL },    // D63
+  { PH_10,        NULL, NULL, NULL },    // D64
+  { PH_9,         NULL, NULL, NULL },    // D65
+  { PA_1,         NULL, NULL, NULL },    // D66
+  { PD_4,         NULL, NULL, NULL },    // D67
+
+  // D68 - D74
+  { PC_6,         NULL, NULL, NULL },    // D68
+  { PI_0,         NULL, NULL, NULL },    // D69
+  { PI_1,         NULL, NULL, NULL },    // D70
+  { PI_2,         NULL, NULL, NULL },    // D71
+  { PI_3,         NULL, NULL, NULL },    // D72
+  { PC_1,         NULL, NULL, NULL },    // D73
+  { PB_12,        NULL, NULL, NULL },    // D74
+  { PD_3,         NULL, NULL, NULL },    // D75
+
+  // A0 - A9
+  { PC_4,         NULL, NULL, NULL },    // A0 (D76)
+  { PC_5,         NULL, NULL, NULL },    // A1 (D77)
+  { PB_0,         NULL, NULL, NULL },    // A2 (D78)
+  { PB_1,         NULL, NULL, NULL },    // A3 (D79)
+  { PC_3,         NULL, NULL, NULL },    // A4 (D80)
+  { PC_2,         NULL, NULL, NULL },    // A5 (D81)
+  { PC_0,         NULL, NULL, NULL },    // A6 (D82)
+  { PA_0,         NULL, NULL, NULL },    // A7 (D83)
+  { PA_4,         NULL, NULL, NULL },    // A12  DAC0 (D84)
+  { PA_5,         NULL, NULL, NULL },    // A13  DAC1 (D85)
+
+  // LEDS
+  { PI_12,        NULL, NULL, NULL },    // D86  LEDR
+  { PJ_13,        NULL, NULL, NULL },    // D87  LEDG
+  { PE_3,         NULL, NULL, NULL },    // D88  LEDB
+
+  // SPI1
+  { PG_9,         NULL, NULL, NULL },    // D89  MISO
+  { PD_7,         NULL, NULL, NULL },    // D90  MOSI
+  { PB_3,         NULL, NULL, NULL },    // D91  SCK
+  { PA_15,        NULL, NULL, NULL },    // D92  CS
+
+  // CAN
+  { PB_5,         NULL, NULL, NULL },    // D93  CAN RX
+  { PB_13,        NULL, NULL, NULL },    // D94  CAN TX
+
+  // WIFI CONTROL
+  { PI_8,         NULL, NULL, NULL },    // D95  WIFI HOST WAKE
+  { PB_10,        NULL, NULL, NULL },    // D96  WIFI ON
+
+  // BLE CONTROL
+  { PH_7,         NULL, NULL, NULL },    // D97  BLE DEVICE WAKE
+  { PG_3,         NULL, NULL, NULL },    // D98  BLE HOST WAKE
+  { PA_10,        NULL, NULL, NULL },    // D99  BLE ON
+
+  // BOOT
+  { PC_13,        NULL, NULL, NULL },    // D100 BOOT0
+
+  // I2C1
+  { PB_6_ALT0,    NULL, NULL, NULL },    // D101  SCL1
+  { PH_12,        NULL, NULL, NULL },    // D102  SDA1
+};
+
+extern "C" {
+  unsigned int PINCOUNT_fn() {
+    return (sizeof(g_APinDescription) / sizeof(g_APinDescription[0]));
+  }
+}
+
+#include "drivers/I2C.h"
+
+void fixup3V1Rail() {
+  mbed::I2C i2c(PB_7, PB_6);
+  char data[2];
+  data[0]=0x42;
+  data[1]=(1);
+  i2c.write(8 << 1, data, sizeof(data));
+}
+
+void initVariant() {
+  RTCHandle.Instance = RTC;
+  // Turn off LED from bootloader
+  pinMode(LEDG, OUTPUT);
+  digitalWrite(LEDG, HIGH);
+  // Disable the FMC bank1 (enabled after reset)
+  // See https://github.com/STMicroelectronics/STM32CubeH7/blob/beced99ac090fece04d1e0eb6648b8075e156c6c/Projects/STM32H747I-DISCO/Applications/OpenAMP/OpenAMP_RTOS_PingPong/Common/Src/system_stm32h7xx.c#L215
+  FMC_Bank1_R->BTCR[0] = 0x000030D2;
+}
+
+#ifdef SERIAL_CDC
+
+static void utox8(uint32_t val, uint8_t* s) {
+  for (int i = 0; i < 16; i=i+2) {
+    int d = val & 0XF;
+    val = (val >> 4);
+
+    s[15 - i -1] = d > 9 ? 'A' + d - 10 : '0' + d;
+    s[15 - i] = '\0';
+  }
+}
+
+uint8_t getUniqueSerialNumber(uint8_t* name) {
+  utox8(HAL_GetUIDw0(), &name[0]);
+  utox8(HAL_GetUIDw1(), &name[16]);
+  utox8(HAL_GetUIDw2(), &name[32]);
+  return 48;
+}
+
+void _ontouch1200bps_() {
+  HAL_RTCEx_BKUPWrite(&RTCHandle, RTC_BKP_DR0, 0xDF59);
+  NVIC_SystemReset();
+}
+
+#endif
