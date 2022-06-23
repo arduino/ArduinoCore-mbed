fa5a1e1c14123e1e886a6ddc2fd99a85e34c5ef6
Martino Facchin <m.facchin@arduino.cc>
2022-06-23 11:03:05 +0200

    nano33ble: disable UART0 only if enabled by bootloader

--- variants/ARDUINO_NANO33BLE/variant.cpp
+++ variants/ARDUINO_NANO33BLE/variant.cpp
@@ -165,10 +165,12 @@ void initVariant() {
   digitalWrite(PIN_ENABLE_I2C_PULLUP, HIGH);
 
   // Disable UARTE0 which is initially enabled by the bootloader
-  nrf_uarte_task_trigger(NRF_UARTE0, NRF_UARTE_TASK_STOPRX); 
-  while (!nrf_uarte_event_check(NRF_UARTE0, NRF_UARTE_EVENT_RXTO)) ; 
-  NRF_UARTE0->ENABLE = 0; 
-  NRF_UART0->ENABLE = 0; 
+  if (nrf_uarte_event_check(NRF_UARTE0, NRF_UARTE_EVENT_RXTO)) {
+    nrf_uarte_task_trigger(NRF_UARTE0, NRF_UARTE_TASK_STOPRX);
+    while (!nrf_uarte_event_check(NRF_UARTE0, NRF_UARTE_EVENT_RXTO));
+    NRF_UARTE0->ENABLE = 0;
+    NRF_UART0->ENABLE = 0;
+  }
 
   NRF_PWM_Type* PWM[] = {
     NRF_PWM0, NRF_PWM1, NRF_PWM2
