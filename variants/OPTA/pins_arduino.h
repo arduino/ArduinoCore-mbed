e9ffe999cd8daf27f6cda5c338ff300e077347cc
Martino Facchin <m.facchin@arduino.cc>
2022-09-16 15:36:16 +0200

    Add Opta target

---
+++ variants/OPTA/pins_arduino.h
@@ -0,0 +1,169 @@
+#pragma once
+#include <macros.h>
+#include "PeripheralPins.h"
+
+#ifndef __PINS_ARDUINO__
+#define __PINS_ARDUINO__
+
+#ifdef __cplusplus
+extern "C" unsigned int PINCOUNT_fn();
+extern "C" bool isBetaBoard();
+#endif
+
+// Booting
+// ----
+void bootM4();
+
+extern PinName digitalPinToPinName(pin_size_t P);
+
+// Pin count
+// ----
+#define PINS_COUNT           (PINCOUNT_fn())
+#define NUM_DIGITAL_PINS     (12u)
+#define NUM_ANALOG_INPUTS    (8u)
+#define NUM_ANALOG_OUTPUTS   (4u)
+
+// LEDs
+// ----
+#define PIN_LED     (24u)
+#define LED_BUILTIN PIN_LED
+#define LEDR        (23u)
+#define LEDG        (24u)
+#define LEDB        (25u)
+
+// Analog pins
+// -----------
+#define PIN_A0  (15u)
+#define PIN_A1  (16u)
+#define PIN_A2  (17u)
+#define PIN_A3  (18u)
+#define PIN_A4  (19u)
+#define PIN_A5  (20u)
+#define PIN_A6  (21u)
+#define PIN_A7  (22u)
+#define PIN_A8  (26u)
+#define PIN_A9  (27u)
+#define PIN_A10 (28u)
+#define PIN_A11 (29u)
+
+static const uint8_t A0  = PIN_A0;
+static const uint8_t A1  = PIN_A1;
+static const uint8_t A2  = PIN_A2;
+static const uint8_t A3  = PIN_A3;
+static const uint8_t A4  = PIN_A4;
+static const uint8_t A5  = PIN_A5;
+static const uint8_t A6  = PIN_A6;
+static const uint8_t A7  = PIN_A7;
+#define ADC_RESOLUTION 12
+
+// Digital pins
+// -----------
+#define D0  (0u)
+#define D1  (1u)
+#define D2  (2u)
+#define D3  (3u)
+#define D4  (4u)
+#define D5  (5u)
+#define D6  (6u)
+#define D7  (7u)
+#define D8  (6u)
+#define D9  (9u)
+#define D10 (10u)
+#define D11 (11u)
+#define D12 (12u)
+#define D13 (13u)
+#define D14 (14u)
+#define D15 #error Pin cannot be used as digital pin.
+#define D16 #error Pin cannot be used as digital pin.
+#define D17 #error Pin cannot be used as digital pin.
+#define D18 #error Pin cannot be used as digital pin.
+#define D19 (19u)
+#define D20 (20u)
+#define D21 (21u)
+
+//DACs
+#define DAC           (A6)
+
+// Serial
+#define PIN_SERIAL_RX (13ul)
+#define PIN_SERIAL_TX (14ul)
+
+// SPI
+#define PIN_SPI_MISO  (10u)
+#define PIN_SPI_MOSI  (8u)
+#define PIN_SPI_SCK   (9u)
+#define PIN_SPI_SS    (7u)
+
+static const uint8_t SS   = PIN_SPI_SS;   // SPI Slave SS not used. Set here only for reference.
+static const uint8_t MOSI = PIN_SPI_MOSI;
+static const uint8_t MISO = PIN_SPI_MISO;
+static const uint8_t SCK  = PIN_SPI_SCK;
+
+// QSPI
+#define QSPI_SO0        PD_11
+#define QSPI_SO1        PD_12
+#define QSPI_SO2        PE_2
+#define QSPI_SO3        PD_13
+#define QSPI_SCK        PB_2
+#define QSPI_CS         PG_6
+
+// Wire
+#define PIN_WIRE_SDA        (11u)
+#define PIN_WIRE_SCL        (12u)
+
+#define SERIAL_HOWMANY		2
+#define SERIAL1_TX			(digitalPinToPinName(PIN_SERIAL_TX))
+#define SERIAL1_RX			(digitalPinToPinName(PIN_SERIAL_RX))
+
+#define SERIAL2_TX			PB_10
+#define SERIAL2_RX			PB_11
+
+#define SERIAL_CDC			1
+#define HAS_UNIQUE_ISERIAL_DESCRIPTOR
+#define BOARD_VENDORID		0x2341
+#define BOARD_PRODUCTID		0x0264
+#define BOARD_NAME			"Arduino Opta"
+
+#define DFU_MAGIC_SERIAL_ONLY_RESET   0xb0
+
+uint8_t getUniqueSerialNumber(uint8_t* name);
+void _ontouch1200bps_();
+
+#define WIRE_HOWMANY		2
+
+#define I2C_SDA				(digitalPinToPinName(PIN_WIRE_SDA))
+#define I2C_SCL				(digitalPinToPinName(PIN_WIRE_SCL))
+
+#define I2C_SDA_INTERNAL	(PB_7)
+#define I2C_SCL_INTERNAL	(PB_6)
+#define I2C_SDA1			I2C_SDA_INTERNAL
+#define I2C_SCL1			I2C_SCL_INTERNAL
+
+#define SPI_HOWMANY			1
+
+#define SPI_MISO			(digitalPinToPinName(PIN_SPI_MISO))
+#define SPI_MOSI			(digitalPinToPinName(PIN_SPI_MOSI))
+#define SPI_SCK				(digitalPinToPinName(PIN_SPI_SCK))
+
+#define digitalPinToPort(P)		(digitalPinToPinName(P)/32)
+
+#define SERIAL_PORT_USBVIRTUAL      SerialUSB
+#define SERIAL_PORT_MONITOR         SerialUSB
+#define SERIAL_PORT_HARDWARE        Serial1
+#define SERIAL_PORT_HARDWARE_OPEN   Serial2
+#define RS485_SERIAL_PORT           Serial2
+
+#define RS485_DEFAULT_TX_PIN        SERIAL2_TX
+#define RS485_DEFAULT_DE_PIN        PB_14
+#define RS485_DEFAULT_RE_PIN        RS485_DEFAULT_DE_PIN
+
+#define SerialLoRa		Serial3
+#define LORA_BOOT0      (PG_7)
+#define LORA_RESET      (PC_7)
+#define LORA_IRQ_DUMB   (PJ_11)
+
+#define CRYPTO_WIRE		Wire1
+
+#define USB_MAX_POWER	(500)
+
+#endif //__PINS_ARDUINO__
---
+++ variants/OPTA/variant.cpp
@@ -0,0 +1,273 @@
+#include "Arduino.h"
+#include "pinDefinitions.h"
+
+RTC_HandleTypeDef RTCHandle;
+
+AnalogPinDescription g_AAnalogPinDescription[] = {
+  { PA_0C,        NULL },    // A0    ADC2_INP0
+  { PA_1C,        NULL },    // A1    ADC2_INP1
+  { PC_2C,        NULL },    // A2    ADC3_INP0
+  { PC_3C,        NULL },    // A3    ADC3_INP1
+  { PC_2_ALT0,    NULL },    // A4    ADC1_INP12
+  { PC_3_ALT2,    NULL },    // A5    ADC2_INP13
+  { PA_4,         NULL },    // A6    ADC1_INP18
+  { PA_6,         NULL }     // A7    ADC1_INP7
+};
+
+PinDescription g_APinDescription[] = {
+  // D0 - D7
+  { PI_6,         NULL, NULL, NULL },    // D0
+  { PI_5,         NULL, NULL, NULL },    // D1
+  { PI_7,         NULL, NULL, NULL },    // D2
+  { PI_4,         NULL, NULL, NULL },    // D3
+  { PC_7,         NULL, NULL, NULL },    // D4
+  { PC_6,         NULL, NULL, NULL },    // D5
+  { PA_8,         NULL, NULL, NULL },    // D6
+  { PI_0,         NULL, NULL, NULL },    // D7
+
+  // D8 - D14
+  { PI_3,         NULL, NULL, NULL },    // D8
+  { PI_1,         NULL, NULL, NULL },    // D9
+  { PI_2,         NULL, NULL, NULL },    // D10
+  { PH_8,         NULL, NULL, NULL },    // D11
+  { PH_7,         NULL, NULL, NULL },    // D12
+  { PG_9,         NULL, NULL, NULL },    // D13
+  { PG_14,        NULL, NULL, NULL },    // D14
+
+  // A0 - A6
+  { PA_0C,        NULL, NULL, NULL },    // A0    ADC2_INP0
+  { PC_2C,        NULL, NULL, NULL },    // A1    ADC2_INP1
+  { PF_12,        NULL, NULL, NULL },    // A2    ADC3_INP0
+  { PB_0,         NULL, NULL, NULL },    // A3    ADC3_INP1
+  { PF_10,        NULL, NULL, NULL },    // A4    ADC1_INP12
+  { PF_8,         NULL, NULL, NULL },    // A5    ADC1_INP13
+  { PF_6,         NULL, NULL, NULL },    // A6    ADC1_INP18
+  { PF_4,         NULL, NULL, NULL },    // A7    ADC1_INP7
+
+  // LEDS
+  { PH_11,        NULL, NULL, NULL },    // LEDR
+  { PH_12,        NULL, NULL, NULL },    // LEDG
+  { PE_5,         NULL, NULL, NULL },    // LEDB  //LED_BLE
+  { PI_6,         NULL, NULL, NULL },    // A8
+  { PI_5,         NULL, NULL, NULL },    // A9
+  { PI_7,         NULL, NULL, NULL },    // A10
+  { PI_4,         NULL, NULL, NULL },    // A11
+  { PG_14,        NULL, NULL, NULL },
+  { PA_5,         NULL, NULL, NULL },
+  { PA_6,         NULL, NULL, NULL },
+  { PA_7,         NULL, NULL, NULL },
+  { PA_8,         NULL, NULL, NULL },
+  { PA_9,         NULL, NULL, NULL },
+  { PA_10,        NULL, NULL, NULL },
+  { PA_11,        NULL, NULL, NULL },
+  { PA_12,        NULL, NULL, NULL },
+  { PA_13,        NULL, NULL, NULL },
+  { PA_14,        NULL, NULL, NULL },
+  { PA_15,        NULL, NULL, NULL },
+  { PB_0,         NULL, NULL, NULL },
+  { PB_1,         NULL, NULL, NULL },
+  { PB_2,         NULL, NULL, NULL },
+  { PB_3,         NULL, NULL, NULL },
+  { PB_4,         NULL, NULL, NULL },
+  { PB_5,         NULL, NULL, NULL },
+  { PB_6,         NULL, NULL, NULL },
+  { PB_7,         NULL, NULL, NULL },
+  { PB_8,         NULL, NULL, NULL },
+  { PB_9,         NULL, NULL, NULL },
+  { PB_10,        NULL, NULL, NULL },
+  { PB_11,        NULL, NULL, NULL },
+  { PB_12,        NULL, NULL, NULL },
+  { PB_13,        NULL, NULL, NULL },
+  { PB_14,        NULL, NULL, NULL },
+  { PB_15,        NULL, NULL, NULL },
+  { PC_0,         NULL, NULL, NULL },
+  { PC_1,         NULL, NULL, NULL },
+  { PC_2,         NULL, NULL, NULL },
+  { PC_3,         NULL, NULL, NULL },
+  { PC_4,         NULL, NULL, NULL },
+  { PC_5,         NULL, NULL, NULL },
+  { PC_6,         NULL, NULL, NULL },
+  { PC_7,         NULL, NULL, NULL },
+  { PC_8,         NULL, NULL, NULL },
+  { PC_9,         NULL, NULL, NULL },
+  { PC_10,        NULL, NULL, NULL },
+  { PC_11,        NULL, NULL, NULL },
+  { PC_12,        NULL, NULL, NULL },
+  { PC_13,        NULL, NULL, NULL },
+  { PC_14,        NULL, NULL, NULL },
+  { PC_15,        NULL, NULL, NULL },
+  { PD_0,         NULL, NULL, NULL },
+  { PD_1,         NULL, NULL, NULL },
+  { PD_2,         NULL, NULL, NULL },
+  { PD_3,         NULL, NULL, NULL },
+  { PD_4,         NULL, NULL, NULL },
+  { PD_5,         NULL, NULL, NULL },
+  { PD_6,         NULL, NULL, NULL },
+  { PD_7,         NULL, NULL, NULL },
+  { PD_8,         NULL, NULL, NULL },
+  { PD_9,         NULL, NULL, NULL },
+  { PD_10,        NULL, NULL, NULL },
+  { PD_11,        NULL, NULL, NULL },
+  { PD_12,        NULL, NULL, NULL },
+  { PD_13,        NULL, NULL, NULL },
+  { PD_14,        NULL, NULL, NULL },
+  { PD_15,        NULL, NULL, NULL },
+  { PE_0,         NULL, NULL, NULL },
+  { PE_1,         NULL, NULL, NULL },
+  { PE_2,         NULL, NULL, NULL },
+  { PE_3,         NULL, NULL, NULL },
+  { PE_4,         NULL, NULL, NULL },
+  { PE_5,         NULL, NULL, NULL },
+  { PE_6,         NULL, NULL, NULL },
+  { PE_7,         NULL, NULL, NULL },
+  { PE_8,         NULL, NULL, NULL },
+  { PE_9,         NULL, NULL, NULL },
+  { PE_10,        NULL, NULL, NULL },
+  { PE_11,        NULL, NULL, NULL },
+  { PE_12,        NULL, NULL, NULL },
+  { PE_13,        NULL, NULL, NULL },
+  { PE_14,        NULL, NULL, NULL },
+  { PE_15,        NULL, NULL, NULL },
+  { PF_0,         NULL, NULL, NULL },
+  { PF_1,         NULL, NULL, NULL },
+  { PF_2,         NULL, NULL, NULL },
+  { PF_3,         NULL, NULL, NULL },
+  { PF_4,         NULL, NULL, NULL },
+  { PF_5,         NULL, NULL, NULL },
+  { PF_6,         NULL, NULL, NULL },
+  { PF_7,         NULL, NULL, NULL },
+  { PF_8,         NULL, NULL, NULL },
+  { PF_9,         NULL, NULL, NULL },
+  { PF_10,        NULL, NULL, NULL },
+  { PF_11,        NULL, NULL, NULL },
+  { PF_12,        NULL, NULL, NULL },
+  { PF_13,        NULL, NULL, NULL },
+  { PF_14,        NULL, NULL, NULL },
+  { PF_15,        NULL, NULL, NULL },
+  { PG_0,         NULL, NULL, NULL },
+  { PG_1,         NULL, NULL, NULL },
+  { PG_2,         NULL, NULL, NULL },
+  { PG_3,         NULL, NULL, NULL },
+  { PG_4,         NULL, NULL, NULL },
+  { PG_5,         NULL, NULL, NULL },
+  { PG_6,         NULL, NULL, NULL },
+  { PG_7,         NULL, NULL, NULL },
+  { PG_8,         NULL, NULL, NULL },
+  { PG_9,         NULL, NULL, NULL },
+  { PG_10,        NULL, NULL, NULL },
+  { PG_11,        NULL, NULL, NULL },
+  { PG_12,        NULL, NULL, NULL },
+  { PG_13,        NULL, NULL, NULL },
+  { PG_14,        NULL, NULL, NULL },
+  { PG_15,        NULL, NULL, NULL },
+  { PH_0,         NULL, NULL, NULL },
+  { PH_1,         NULL, NULL, NULL },
+  { PH_2,         NULL, NULL, NULL },
+  { PH_3,         NULL, NULL, NULL },
+  { PH_4,         NULL, NULL, NULL },
+  { PH_5,         NULL, NULL, NULL },
+  { PH_6,         NULL, NULL, NULL },
+  { PH_7,         NULL, NULL, NULL },
+  { PH_8,         NULL, NULL, NULL },
+  { PH_9,         NULL, NULL, NULL },
+  { PH_10,        NULL, NULL, NULL },
+  { PH_11,        NULL, NULL, NULL },
+  { PH_12,        NULL, NULL, NULL },
+  { PH_13,        NULL, NULL, NULL },
+  { PH_14,        NULL, NULL, NULL },
+  { PH_15,        NULL, NULL, NULL },
+  { PI_0,         NULL, NULL, NULL },
+  { PI_1,         NULL, NULL, NULL },
+  { PI_2,         NULL, NULL, NULL },
+  { PI_3,         NULL, NULL, NULL },
+  { PI_4,         NULL, NULL, NULL },
+  { PI_5,         NULL, NULL, NULL },
+  { PI_6,         NULL, NULL, NULL },
+  { PI_7,         NULL, NULL, NULL },
+  { PI_8,         NULL, NULL, NULL },
+  { PI_9,         NULL, NULL, NULL },
+  { PI_10,        NULL, NULL, NULL },
+  { PI_11,        NULL, NULL, NULL },
+  { PI_12,        NULL, NULL, NULL },
+  { PI_13,        NULL, NULL, NULL },
+  { PI_14,        NULL, NULL, NULL },
+  { PI_15,        NULL, NULL, NULL },
+  { PJ_0,         NULL, NULL, NULL },
+  { PJ_1,         NULL, NULL, NULL },
+  { PJ_2,         NULL, NULL, NULL },
+  { PJ_3,         NULL, NULL, NULL },
+  { PJ_4,         NULL, NULL, NULL },
+  { PJ_5,         NULL, NULL, NULL },
+  { PJ_6,         NULL, NULL, NULL },
+  { PJ_7,         NULL, NULL, NULL },
+  { PJ_8,         NULL, NULL, NULL },
+  { PJ_9,         NULL, NULL, NULL },
+  { PJ_10,        NULL, NULL, NULL },
+  { PJ_11,        NULL, NULL, NULL },
+  { PJ_12,        NULL, NULL, NULL },
+  { PJ_13,        NULL, NULL, NULL },
+  { PJ_14,        NULL, NULL, NULL },
+  { PJ_15,        NULL, NULL, NULL },
+  { PK_0,         NULL, NULL, NULL },
+  { PK_1,         NULL, NULL, NULL },
+  { PK_2,         NULL, NULL, NULL },
+  { PK_3,         NULL, NULL, NULL },
+  { PK_4,         NULL, NULL, NULL },
+  { PK_5,         NULL, NULL, NULL },
+  { PK_6,         NULL, NULL, NULL },
+  { PK_7,         NULL, NULL, NULL },
+};
+
+extern "C" {
+  unsigned int PINCOUNT_fn() {
+    return (sizeof(g_APinDescription) / sizeof(g_APinDescription[0]));
+  }
+}
+
+#include "drivers/I2C.h"
+
+void fixup3V1Rail() {
+  mbed::I2C i2c(PB_7, PB_6);
+  char data[2];
+  data[0]=0x42;
+  data[1]=(1);
+  i2c.write(8 << 1, data, sizeof(data));
+}
+
+void initVariant() {
+  RTCHandle.Instance = RTC;
+  // Turn off LED from bootloader
+  pinMode(LEDG, OUTPUT);
+  pinMode(LEDR, OUTPUT);
+  digitalWrite(LEDG, LOW);
+  digitalWrite(LEDR, LOW);
+  // Disable the FMC bank1 (enabled after reset)
+  // See https://github.com/STMicroelectronics/STM32CubeH7/blob/beced99ac090fece04d1e0eb6648b8075e156c6c/Projects/STM32H747I-DISCO/Applications/OpenAMP/OpenAMP_RTOS_PingPong/Common/Src/system_stm32h7xx.c#L215
+  FMC_Bank1_R->BTCR[0] = 0x000030D2;
+}
+
+#ifdef SERIAL_CDC
+
+static void utox8(uint32_t val, uint8_t* s) {
+  for (int i = 0; i < 16; i=i+2) {
+    int d = val & 0XF;
+    val = (val >> 4);
+
+    s[15 - i -1] = d > 9 ? 'A' + d - 10 : '0' + d;
+    s[15 - i] = '\0';
+  }
+}
+
+uint8_t getUniqueSerialNumber(uint8_t* name) {
+  utox8(HAL_GetUIDw0(), &name[0]);
+  utox8(HAL_GetUIDw1(), &name[16]);
+  utox8(HAL_GetUIDw2(), &name[32]);
+  return 48;
+}
+
+void _ontouch1200bps_() {
+  HAL_RTCEx_BKUPWrite(&RTCHandle, RTC_BKP_DR0, 0xDF59);
+  NVIC_SystemReset();
+}
+
+#endif
