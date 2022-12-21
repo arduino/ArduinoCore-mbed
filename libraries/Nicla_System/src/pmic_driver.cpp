1f490b7b058bb0ae37c5fe0b43f6c957767af7d8
Martino Facchin <m.facchin@arduino.cc>
2022-12-21 13:03:21 +0100

    Nicla_System: statically allocate CD pin

--- libraries/Nicla_System/src/pmic_driver.cpp
+++ libraries/Nicla_System/src/pmic_driver.cpp
@@ -2,6 +2,9 @@
 #include <I2C.h>
 #include "BQ25120A.h"
 #include "Nicla_System.h"
+#include "DigitalOut.h"
+
+static mbed::DigitalOut cd(p25);
 
 uint8_t BQ25120A::getStatus()
 {
@@ -11,29 +14,29 @@ uint8_t BQ25120A::getStatus()
 
 void BQ25120A::writeByte(uint8_t address, uint8_t subAddress, uint8_t data)
 {
-  digitalWrite(p25, HIGH);
+  cd = 1;
   nicla::i2c_mutex.lock();
   Wire1.beginTransmission(address);
   Wire1.write(subAddress);
   Wire1.write(data);
   Wire1.endTransmission();
   nicla::i2c_mutex.unlock();
-  digitalWrite(p25, LOW);
+  cd = 0;
 }
 
 uint8_t BQ25120A::readByte(uint8_t address, uint8_t subAddress)
 {
-  digitalWrite(p25, HIGH);
+  cd = 1;
   nicla::i2c_mutex.lock();
   Wire1.beginTransmission(address);
   Wire1.write(subAddress);
   Wire1.endTransmission(false);
   Wire1.requestFrom(address, 1);
-  uint32_t timeout = 1000;
+  uint32_t timeout = 100;
   uint32_t start_time = millis();
   while(!Wire1.available() && (millis() - start_time) < timeout) {}
   uint8_t ret = Wire1.read();
   nicla::i2c_mutex.unlock();
-  digitalWrite(p25, LOW);
+  cd = 0;
   return ret;
 }
