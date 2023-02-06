c8e5f11d4f77f54267f58c27999b888e81b4115d
Ali Jahangiri <75624145+aliphys@users.noreply.github.com>
2023-02-06 09:55:16 +0100

    Update libraries/Nicla_System/examples/Blink_Nicla/Blink_Nicla.ino

--- libraries/Nicla_System/examples/Blink_Nicla/Blink_Nicla.ino
+++ libraries/Nicla_System/examples/Blink_Nicla/Blink_Nicla.ino
@@ -40,7 +40,7 @@
 //This sketch is only for the Nicla Sense ME, not the Nicla Vision
 #ifdef ARDUINO_NICLA_VISION
   #error "Run the standard Blink.ino sketch for the Nicla Vision"
-
+#endif
 
 // Intialise library which communicates with RGB driver
 // Functions accessible under 'nicla' namespace
