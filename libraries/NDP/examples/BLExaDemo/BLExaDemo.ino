8593ab99713c662060cd02ca95c3ef25b2fd1bce
Martino Facchin <m.facchin@arduino.cc>
2023-02-23 09:54:39 +0100

    nicla_voice: update firmwares for BLExaDemo example

--- libraries/NDP/examples/BLExaDemo/BLExaDemo.ino
+++ libraries/NDP/examples/BLExaDemo/BLExaDemo.ino
@@ -56,9 +56,9 @@ void setup() {
   NDP.onError(ledRedBlink);
   NDP.onMatch(alertViaBLE);
   NDP.onEvent(ledGreenOn);
-  NDP.begin("mcu_fw_120_v90.synpkg");
-  NDP.load("dsp_firmware_v90.synpkg");
-  NDP.load("alexa_334_NDP120_B0_v11_v90.synpkg");
+  NDP.begin("mcu_fw_120_v91.synpkg");
+  NDP.load("dsp_firmware_v91.synpkg");
+  NDP.load("alexa_334_NDP120_B0_v11_v91.synpkg");
   NDP.turnOnMicrophone();
   NDP.interrupts();
 
