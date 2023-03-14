78b1e0588ad88a648c7455cc062f76bf47a318e6
Leonardo Cavagnis <leonardo.cavagnis@hotmail.it>
2023-03-14 12:53:42 +0100

    add functions to draw elements in portenta_video lib

--- libraries/Portenta_Video/src/Portenta_Video.cpp
+++ libraries/Portenta_Video/src/Portenta_Video.cpp
@@ -1,10 +1,39 @@
 #include "Portenta_Video.h"
+
 #include "Portenta_lvgl.h"
+#include "anx7625.h"
+#include "dsi.h"
 
-arduino::Portenta_Video::Portenta_Video() {
-
-}
+arduino::Portenta_Video::Portenta_Video() { }
 
 int arduino::Portenta_Video::begin() {
     portenta_init_video();
+    
+    _displayWidth   = stm32_getXSize();
+    _displayHeight  = stm32_getYSize();
+
+    _currFrameBufferAddr = getCurrentFrameBuffer();
+}
+
+void arduino::Portenta_Video::fillScreen(uint32_t color) {
+    stm32_LCD_Clear(color); 
+}
+
+void arduino::Portenta_Video::updateDisplay() {
+    _currFrameBufferAddr = getNextFrameBuffer();
+}
+
+void arduino::Portenta_Video::drawImage(void *imgBuffer, uint32_t imgWidth, uint32_t imgHeight, uint32_t posWidht, uint32_t posHeight) {
+    uint32_t offsetPos = 0;
+
+    offsetPos = (posWidht + (_displayWidth * posHeight)) * sizeof(uint16_t);
+    stm32_LCD_DrawImage(imgBuffer, (void *)(_currFrameBufferAddr + offsetPos), imgWidth, imgHeight, DMA2D_INPUT_RGB565);
+}
+
+uint32_t arduino::Portenta_Video::getWidthSize() {
+    return _displayWidth;
+}
+
+uint32_t arduino::Portenta_Video::getHeightSize() {
+    return _displayHeight;
 }
--- libraries/Portenta_Video/src/Portenta_Video.h
+++ libraries/Portenta_Video/src/Portenta_Video.h
@@ -11,7 +11,15 @@ namespace arduino {
     public:
         Portenta_Video();
         int begin();
+        void fillScreen(uint32_t color);
+        void drawImage(void *imgBuffer, uint32_t imgWidth, uint32_t imgHeight, uint32_t posWidht, uint32_t posHeight);
+        void updateDisplay();
+        uint32_t getWidthSize();
+        uint32_t getHeightSize();
     private:
+        uint32_t _currFrameBufferAddr;
+        uint32_t _displayWidth;
+        uint32_t _displayHeight;
     };  
 
 }
