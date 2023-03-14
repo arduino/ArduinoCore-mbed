b5b3752df4740b0f9412aceedf8f95f46336c2fd
Leonardo Cavagnis <leonardo.cavagnis@hotmail.it>
2023-03-14 12:58:16 +0100

    refactoring arduinoLogo with new portenta_video apis

--- libraries/Portenta_Video/examples/ArduinoLogo/ArduinoLogo.ino
+++ libraries/Portenta_Video/examples/ArduinoLogo/ArduinoLogo.ino
@@ -3,25 +3,45 @@
 #include "image.h"
 
 // Alternatively, any raw RGB565 image can be included on demand using this macro
+// Online image converter: https://lvgl.io/tools/imageconverter (Output format: Binary RGB565)
 /*
 #define INCBIN_PREFIX
 #include "incbin.h"
 INCBIN(test, "/home/user/Downloads/test.bin");
 */
 
-int offset;
+
+Portenta_Video Display;
 
 void setup() {
-  portenta_init_video();
-
-  stm32_LCD_Clear(0);
-  stm32_LCD_Clear(0);
-
-  offset = ((stm32_getXSize() - 300)) + (stm32_getXSize() * (stm32_getYSize() - 300) / 2) * sizeof(uint16_t);
+  Display.begin();
 }
 
 void loop() {
   // Replace texture_raw with testData if using the INCBIN method
   // Also, replace 300x300 resolution with the actual one
-  stm32_LCD_DrawImage((void*)texture_raw, (void *)(getNextFrameBuffer() + offset), 300, 300, DMA2D_INPUT_RGB565);
+
+  Display.fillScreen(0x0000FF00);
+  Display.drawImage((void*)texture_raw, 300, 300, 0, 0);
+  Display.updateDisplay();
+
+  delay(1000);
+
+  Display.fillScreen(0x0000FF00);
+  Display.drawImage((void*)texture_raw, 300, 300, 0, (Display.getHeightSize() - 300));
+  Display.updateDisplay();
+
+  delay(1000);
+
+  Display.fillScreen(0x0000FF00);
+  Display.drawImage((void*)texture_raw, 300, 300, (Display.getWidthSize() - 300), (Display.getHeightSize() - 300));
+  Display.updateDisplay();
+
+  delay(1000);
+
+  Display.fillScreen(0x0000FF00);
+  Display.drawImage((void*)texture_raw, 300, 300, (Display.getWidthSize() - 300), 0);
+  Display.updateDisplay();
+
+  delay(1000);
 }
