e5de3b2d3ba5e6b4edbbe50de1258134b501ef32
Leonardo Cavagnis <leonardo.cavagnis@hotmail.it>
2023-03-15 10:52:34 +0100

    refactoring args and fun name in portenta_video

--- libraries/Portenta_Video/src/Portenta_Video.cpp
+++ libraries/Portenta_Video/src/Portenta_Video.cpp
@@ -20,37 +20,37 @@ void arduino::Portenta_Video::clear(){
     fillScreen(PV_COLOR_BLACK);
 }
 
-void arduino::Portenta_Video::updateDisplay() {
+void arduino::Portenta_Video::update() {
     _currFrameBufferAddr = getNextFrameBuffer();
 }
 
-void arduino::Portenta_Video::drawImage(void *imgBuffer, uint32_t imgWidth, uint32_t imgHeight, uint32_t posWidht, uint32_t posHeight) {
+void arduino::Portenta_Video::drawImage(uint32_t x, uint32_t y, void *img, uint32_t width, uint32_t height) {
     uint32_t offsetPos = 0;
 
-    offsetPos = (posWidht + (_displayWidth * posHeight)) * sizeof(uint16_t);
-    stm32_LCD_DrawImage(imgBuffer, (void *)(_currFrameBufferAddr + offsetPos), imgWidth, imgHeight, DMA2D_INPUT_RGB565);
+    offsetPos = (x + (_displayWidth * y)) * sizeof(uint16_t);
+    stm32_LCD_DrawImage(img, (void *)(_currFrameBufferAddr + offsetPos), width, height, DMA2D_INPUT_RGB565);
 }
 
-uint32_t arduino::Portenta_Video::getWidthSize() {
+uint32_t arduino::Portenta_Video::getWidth() {
     return _displayWidth;
 }
 
-uint32_t arduino::Portenta_Video::getHeightSize() {
+uint32_t arduino::Portenta_Video::getHeight() {
     return _displayHeight;
 }
 
-void arduino::Portenta_Video::drawFilledRectangle(uint32_t rectWidth, uint32_t rectHeight, uint32_t posWidht, uint32_t posHeight, uint32_t color){
+void arduino::Portenta_Video::drawFilledRectangle(uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t color) {
     uint32_t offsetPos = 0;
 
-    offsetPos = (posWidht + (_displayWidth * posHeight)) * sizeof(uint16_t);
-    stm32_LCD_FillArea((void *)(_currFrameBufferAddr + offsetPos), rectWidth, rectHeight, color);
+    offsetPos = (x + (_displayWidth * y)) * sizeof(uint16_t);
+    stm32_LCD_FillArea((void *)(_currFrameBufferAddr + offsetPos), width, height, color);
 }
 
-void arduino::Portenta_Video::drawRectangle(uint32_t rectWidth, uint32_t rectHeight, uint32_t posWidht, uint32_t posHeight, uint32_t color){
-  int x1 = posWidht;                    // Top-left corner x-coordinate
-  int y1 = posHeight;                   // Top-left corner y-coordinate
-  int x2 = posWidht + rectWidth - 1;    // Bottom-right corner x-coordinate
-  int y2 = posHeight + rectHeight - 1;  // Bottom-right corner y-coordinate
+void arduino::Portenta_Video::drawRectangle(uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t color) {
+  int x1 = x;               // Top-left corner x-coordinate
+  int y1 = y;               // Top-left corner y-coordinate
+  int x2 = x + width - 1;   // Bottom-right corner x-coordinate
+  int y2 = y + height - 1;  // Bottom-right corner y-coordinate
 
   drawLine(x1, y1, x2, y1, color); // Top
   drawLine(x1, y2, x2, y2, color);// Bottom
@@ -58,11 +58,11 @@ void arduino::Portenta_Video::drawRectangle(uint32_t rectWidth, uint32_t rectHei
   drawLine(x2, y1, x2, y2, color); // Right
 }
 
-void arduino::Portenta_Video::drawPixel(uint32_t posWidht, uint32_t posHeight, uint32_t color) {
-    drawFilledRectangle(1, 1, posWidht, posHeight, color);
+void arduino::Portenta_Video::drawPixel(uint32_t x, uint32_t y, uint32_t color) {
+    drawFilledRectangle(x, y, 1, 1, color);
 }
 
-void arduino::Portenta_Video::drawLine(int x0, int y0, int x1, int y1, uint32_t color) {
+void arduino::Portenta_Video::drawLine(uint32_t x0, uint32_t y0, uint32_t x1, uint32_t y1, uint32_t color) {
   //Bresenham's line algorithm
   int dx = abs(x1 - x0);
   int dy = abs(y1 - y0);
@@ -135,4 +135,3 @@ void arduino::Portenta_Video::drawCircle(uint32_t centerX, uint32_t centerY, uin
     x++;
   }
 }
-
--- libraries/Portenta_Video/src/Portenta_Video.h
+++ libraries/Portenta_Video/src/Portenta_Video.h
@@ -5,41 +5,39 @@
 #include "anx7625.h"
 #include "dsi.h"
 
-#define PV_COLOR_BLACK          (0x000000)
-#define PV_COLOR_WHITE          (0xFFFFFF)
-#define PV_COLOR_RED            (0xFF0000)
-#define PV_COLOR_LIME           (0x00FF00)
-#define PV_COLOR_BLUE           (0x0000FF)
-#define PV_COLOR_YELLOW         (0xFFFF00)
-#define PV_COLOR_CYAN           (0x00FFFF)
-#define PV_COLOR_MAGENTA        (0xFF00FF)
-#define PV_COLOR_SILVER         (0xC0C0C0)
-#define PV_COLOR_GRAY           (0x808080)
-#define PV_COLOR_MAROON         (0x800000)
-#define PV_COLOR_OLIVE          (0x808000)
-#define PV_COLOR_GREEN          (0x008000)
-#define PV_COLOR_PURPLE         (0x800080)
-#define PV_COLOR_TEAL           (0x008080)
-#define PV_COLOR_NAVY           (0x000080)
+#define PV_COLOR_BLACK      (0x000000)
+#define PV_COLOR_WHITE      (0xFFFFFF)
+#define PV_COLOR_RED        (0xFF0000)
+#define PV_COLOR_GREEN      (0x00FF00)
+#define PV_COLOR_BLUE       (0x0000FF)
+#define PV_COLOR_YELLOW     (0xFFFF00)
+#define PV_COLOR_MAGENTA    (0xFF00FF)
+#define PV_COLOR_CYAN       (0x00FFFF)
+#define PV_COLOR_ORANGE     (0xFFA500)
+#define PV_COLOR_PINK       (0xFFC0CB)
+#define PV_COLOR_PURPLE     (0x800080)
+#define PV_COLOR_GRAY       (0x808080)
 
 namespace arduino {
 
     class Portenta_Video {
     public:
         Portenta_Video();
-        int begin();
-        void fillScreen(uint32_t color);
-        void drawImage(void *imgBuffer, uint32_t imgWidth, uint32_t imgHeight, uint32_t posWidht, uint32_t posHeight);
-        void drawPixel(uint32_t posWidht, uint32_t posHeight, uint32_t color);
-        void drawLine(int x0, int y0, int x1, int y1, uint32_t color);
-        void drawFilledRectangle(uint32_t rectWidth, uint32_t rectHeight, uint32_t posWidht, uint32_t posHeight, uint32_t color);
-        void drawRectangle(uint32_t rectWidth, uint32_t rectHeight, uint32_t posWidht, uint32_t posHeight, uint32_t color);
-        void drawFilledCircle(uint32_t centerX, uint32_t centerY, uint32_t radius, uint32_t color);
+
+        int         begin();
+        void        fillScreen(uint32_t color);
+        void        clear();
+        void        update();
+        uint32_t    getWidth();
+        uint32_t    getHeight();
+
+        void drawImage(uint32_t x, uint32_t y, void *img, uint32_t width, uint32_t height) ;
+        void drawPixel(uint32_t x, uint32_t y, uint32_t color);
+        void drawLine(uint32_t x0, uint32_t y0, uint32_t x1, uint32_t y1, uint32_t color);
+        void drawRectangle(uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t color);
+        void drawFilledRectangle(uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t color);
         void drawCircle(uint32_t centerX, uint32_t centerY, uint32_t radius, uint32_t color);
-        void clear();
-        void updateDisplay();
-        uint32_t getWidthSize();
-        uint32_t getHeightSize();
+        void drawFilledCircle(uint32_t centerX, uint32_t centerY, uint32_t radius, uint32_t color);
     private:
         uint32_t _currFrameBufferAddr;
         uint32_t _displayWidth;
