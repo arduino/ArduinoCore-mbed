d1dce41639123656fbd0d899b9f6d266ecad9103
Leonardo Cavagnis <leonardo.cavagnis@hotmail.it>
2023-03-24 14:23:24 +0100

    Add ArduinoLogo example (draw image with H7_Video lib)

--- libraries/Portenta_Video/examples/ArduinoLogo/ArduinoLogo.ino
+++ libraries/Portenta_Video/examples/ArduinoLogo/ArduinoLogo.ino
@@ -1,6 +1,14 @@
-#include "Portenta_Video.h"
-#include "image.h"
+/*
+  ArduinoLogo
 
+  created DD MMM YYYY
+  by Leonardo Cavagnis
+*/
+
+#include "ArduinoGraphics.h"
+#include "H7_Video.h" // H7_Video depends on ArduinoGraphics
+
+#include "img_arduinologo.h"
 // Alternatively, any raw RGB565 image can be included on demand using this macro
 // Online image converter: https://lvgl.io/tools/imageconverter (Output format: Binary RGB565)
 /*
@@ -9,15 +17,15 @@
 INCBIN(test, "/home/user/Downloads/test.bin");
 */
 
-Portenta_Video Display;
+H7_Video Display(480, 800);
 
 void setup() {
   Display.begin();
 
-  // Replace texture_raw with testData if using the INCBIN method
-  // Also, replace 300x300 resolution with the actual one
-  Display.drawImage((Display.getWidth() - 300)/2, (Display.getHeight() - 300)/2, (void*)texture_raw, 300, 300);
-  Display.update();
+  Image img_arduinologo(ENCODING_RGB16, (uint8_t *) texture_raw, 300, 300);
+  Display.beginDraw();
+  Display.image(img_arduinologo, (Display.height() - img_arduinologo.width())/2, (Display.width() - img_arduinologo.height())/2);
+  Display.endDraw();
 }
 
 void loop() { }
