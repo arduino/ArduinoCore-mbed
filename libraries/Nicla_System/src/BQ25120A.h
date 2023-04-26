d8fc79883820416d2a89f28451e6a60649c81c90
Sebastian Romero <s.romero@arduino.cc>
2023-04-26 16:52:55 +0200

    nicla-sense-pmic: Use function to toggle Hi-Z mode.

--- libraries/Nicla_System/src/BQ25120A.h
+++ libraries/Nicla_System/src/BQ25120A.h
@@ -4,13 +4,13 @@
 #define BQ25120A_ADDRESS           0x6A
 
 // Register Map
-// https://www.ti.com/lit/ds/symlink/bq25120a.pdf?ts=1610608851953&ref_url=https%253A%252F%252Fwww.startpage.com%252F
+// https://www.ti.com/lit/ds/symlink/bq25120a.pdf
 #define BQ25120A_STATUS             0x00
 #define BQ25120A_FAULTS             0x01
 #define BQ25120A_TS_CONTROL         0x02
 #define BQ25120A_FAST_CHG           0x03
 #define BQ25120A_TERMINATION_CURR   0x04
-#define BQ25120A_BATTERY_CTRL       0x05
+#define BQ25120A_BATTERY_CTRL       0x05 // Battery Voltage Control Register
 #define BQ25120A_SYS_VOUT_CTRL      0x06
 #define BQ25120A_LDO_CTRL           0x07
 #define BQ25120A_PUSH_BUTT_CTRL     0x08
@@ -29,6 +29,21 @@ class BQ25120A
   void writeByte(uint8_t address, uint8_t subAddress, uint8_t data);
   uint8_t readByte(uint8_t address, uint8_t subAddress);
 
+  private:
+  /**
+   * @brief Set the High Impedance Mode Enabled or Disabled.
+   * When enabled, drives the CD pin low to enter high impedance mode.
+   * Note that this only applies when powered with a battery and the condition VIN < VUVLO is met.
+   * When VIN > VUVLO this enables charging instead.
+   * 
+   * When disabled, drives the CD pin high to exit high impedance mode (Active Battery).
+   * When VIN > VUVLO this disables charging.
+   * When exiting this mode, charging resumes if VIN is present, CD is low and charging is enabled.
+   * 
+   * @note The CD pin is internally pulled down.
+   * @param enabled Defines if the high impedance mode should be enabled or disabled.
+   */
+  void setHighImpedanceModeEnabled(bool enabled);
 };
 
 #endif
