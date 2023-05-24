087217152b13cb137bda634f38bb3268885ef4b4
Leonardo Cavagnis <leonardo.cavagnis@hotmail.it>
2023-05-24 15:25:20 +0200

    doc: add doxygen comment

--- libraries/Arduino_H7_Video/src/Arduino_H7_Video.cpp
+++ libraries/Arduino_H7_Video/src/Arduino_H7_Video.cpp
@@ -1,12 +1,26 @@
+/*
+ * Copyright 2023 Arduino SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 /**
-  ******************************************************************************
-  * @file    Arduino_H7_Video.cpp
-  * @author  
-  * @version 
-  * @date    
-  * @brief
-  ******************************************************************************
-  */
+ * @file Arduino_H7_Video.cpp
+ * @author Leonardo Cavagnis
+ * @brief Source file for the Arduino H7 Video library.
+ */
 
  /* Includes ------------------------------------------------------------------*/
 #include "Arduino_H7_Video.h"
