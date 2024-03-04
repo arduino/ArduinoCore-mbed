a12c8c792289dad205f47bfc7295c26249554f43
Martino Facchin <m.facchin@arduino.cc>
2024-03-04 19:42:48 +0100

    lvgl: found a way to automatically select lvgl9

--- libraries/Arduino_H7_Video/src/lv_conf.h
+++ libraries/Arduino_H7_Video/src/lv_conf.h
@@ -1,6 +1,5 @@
 //#if (LVGL_VERSION_MAJOR == 9)
-// TODO: find a way to autodiscover lvgl version
-#if (0)
+#if __has_include("draw/sw/blend/neon/lv_blend_neon.h")
 #include "lv_conf_9.h"
 #else
 #include "lv_conf_8.h"
