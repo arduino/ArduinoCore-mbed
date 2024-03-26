cc76344f833325365bc39c86f3b47a46f2053b3d
Dale Weber <hybotics@hybotics.dev>
2024-03-26 16:08:56 -0700

    Added Portenta C33 (0068) to post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -9,6 +9,7 @@ SUBSYSTEMS=="usb", ATTRS{idVendor}=="2e8a", MODE:="0666"
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="2341", MODE:="0666"
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="1fc9", MODE:="0666"
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="0525", MODE:="0666"
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="0068", MODE:="0666"            #   Portenta C33
 EOF
 }
 
