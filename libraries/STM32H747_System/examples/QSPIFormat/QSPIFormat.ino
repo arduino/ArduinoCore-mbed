b9f1b32b9f6b92c8ed46d22b96c8322b77e0ec32
pennam <m.pennasilico@arduino.cc>
2025-05-05 16:14:38 +0200

    QSPIFormat: use mbed namespace

--- libraries/STM32H747_System/examples/QSPIFormat/QSPIFormat.ino
+++ libraries/STM32H747_System/examples/QSPIFormat/QSPIFormat.ino
@@ -7,14 +7,15 @@
   #error Format QSPI flash by uploading the sketch to the M7 core instead of the M4 core.
 #endif
 
+using namespace mbed;
 
-mbed::BlockDevice* root = mbed::BlockDevice::get_default_instance();
-mbed::MBRBlockDevice wifi_data(root, 1);
-mbed::MBRBlockDevice ota_data(root, 2);
-mbed::MBRBlockDevice user_data(root, 3);
-mbed::FATFileSystem wifi_data_fs("wlan");
-mbed::FATFileSystem ota_data_fs("fs");
-mbed::FileSystem * user_data_fs;
+BlockDevice* root = BlockDevice::get_default_instance();
+MBRBlockDevice wifi_data(root, 1);
+MBRBlockDevice ota_data(root, 2);
+MBRBlockDevice user_data(root, 3);
+FATFileSystem wifi_data_fs("wlan");
+FATFileSystem ota_data_fs("fs");
+FileSystem * user_data_fs;
 
 bool waitResponse() {
   bool confirmation = false;
@@ -60,14 +61,14 @@ void setup() {
   Serial.println("Do you want to proceed? Y/[n]");
 
   if (true == waitResponse()) {
-    mbed::MBRBlockDevice::partition(root, 1, 0x0B, 0, 1024 * 1024);
+    MBRBlockDevice::partition(root, 1, 0x0B, 0, 1024 * 1024);
     if(default_scheme) {
-      mbed::MBRBlockDevice::partition(root, 3, 0x0B, 14 * 1024 * 1024, 14 * 1024 * 1024);
+      MBRBlockDevice::partition(root, 3, 0x0B, 14 * 1024 * 1024, 14 * 1024 * 1024);
       mbed::MBRBlockDevice::partition(root, 2, 0x0B, 1024 * 1024, 14 * 1024 * 1024);
       // use space from 15.5MB to 16 MB for another fw, memory mapped
     } else {
-      mbed::MBRBlockDevice::partition(root, 2, 0x0B, 1024 * 1024, 6 * 1024 * 1024);
-      mbed::MBRBlockDevice::partition(root, 3, 0x0B, 6 * 1024 * 1024, 14 * 1024 * 1024);
+      MBRBlockDevice::partition(root, 2, 0x0B, 1024 * 1024, 6 * 1024 * 1024);
+      MBRBlockDevice::partition(root, 3, 0x0B, 6 * 1024 * 1024, 14 * 1024 * 1024);
       // use space from 15.5MB to 16 MB for another fw, memory mapped
     }
 
