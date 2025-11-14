b9e0f367abec88ecf0ec598afcdd36490681176c
pennam <m.pennasilico@arduino.cc>
2025-11-14 11:17:58 +0100

    ci: use https to access arduino core api repo

--- extras/package_full.sh
+++ extras/package_full.sh
@@ -11,7 +11,7 @@ git reset --hard
 git clean -dxf
 
 # Clone ArduinoCore-api in parent folder and create symlinks
-git clone git@github.com:arduino/ArduinoCore-API.git ../api/
+git clone https://github.com/arduino/ArduinoCore-API.git ../api/
 ln -s ../../../api/api cores/arduino/api
 ./extras/package.sh $flavour
 
